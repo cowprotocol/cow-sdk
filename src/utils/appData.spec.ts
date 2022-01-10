1cf1f89d4570c5079ec15929860d8b15c6314493
Ramiro Vazquez <ramirovazquez@gmail.com>
2022-01-10 07:46:15 -0300

    fix build and add eslint/prettier

--- src/utils/appData.spec.ts
+++ src/utils/appData.spec.ts
@@ -1,68 +1,66 @@
-import {validateAppDataDocument } from './appData'
+import { validateAppDataDocument } from './appData'
 
-const VALID_RESULT = { 
+const VALID_RESULT = {
   result: true,
 }
 
 test('Valid minimal document', async () => {
   const validation = await validateAppDataDocument({
-    "version": "0.1.0",
-    "metadata": {}
+    version: '0.1.0',
+    metadata: {},
   })
-  expect(validation).toEqual(VALID_RESULT);
-});
+  expect(validation).toEqual(VALID_RESULT)
+})
 
 test('Valid minimal document + appCode', async () => {
   const validation = await validateAppDataDocument({
-    "version": "0.1.0",
-    "appCode": "MyApp",
-    "metadata": {}
+    version: '0.1.0',
+    appCode: 'MyApp',
+    metadata: {},
   })
-  expect(validation).toEqual(VALID_RESULT);
-});
-
+  expect(validation).toEqual(VALID_RESULT)
+})
 
 test('Valid minimal document + appCode + referrer', async () => {
   const validation = await validateAppDataDocument({
-    "version": "0.1.0",
-    "appCode": "MyApp",
-    "metadata": {
-      "referrer": {
-        "version": "0.1.0",
-        "address": "0xFEB4acf3df3cDEA7399794D0869ef76A6EfAff52"
-      }
-    }
+    version: '0.1.0',
+    appCode: 'MyApp',
+    metadata: {
+      referrer: {
+        version: '0.1.0',
+        address: '0xFEB4acf3df3cDEA7399794D0869ef76A6EfAff52',
+      },
+    },
   })
-  expect(validation).toEqual(VALID_RESULT);
-});
+  expect(validation).toEqual(VALID_RESULT)
+})
 
 test('Invalid: Bad referrer', async () => {
   const validation = await validateAppDataDocument({
-    "version": "0.1.0",
-    "appCode": "MyApp",
-    "metadata": {
-      "referrer": {
-        "version": "0.1.0",
-        "address": "this is not an ethereum address"
-      }
-    }
+    version: '0.1.0',
+    appCode: 'MyApp',
+    metadata: {
+      referrer: {
+        version: '0.1.0',
+        address: 'this is not an ethereum address',
+      },
+    },
   })
-  expect(validation.result).toBeFalsy();
-});
+  expect(validation.result).toBeFalsy()
+})
 
 test('Invalid: No version', async () => {
   const validation = await validateAppDataDocument({
-    "appCode": "MyApp",
-    "metadata": {}
+    appCode: 'MyApp',
+    metadata: {},
   })
-  expect(validation.result).toBeFalsy();
-});
-
+  expect(validation.result).toBeFalsy()
+})
 
 test('Invalid: No metadata', async () => {
   const validation = await validateAppDataDocument({
-    "version": "0.1.0",
-    "appCode": "MyApp",
+    version: '0.1.0',
+    appCode: 'MyApp',
   })
-  expect(validation.result).toBeFalsy();
-});
+  expect(validation.result).toBeFalsy()
+})
--- src/utils/appData.ts
+++ src/utils/appData.ts
@@ -1,34 +1,32 @@
-import Ajv from 'ajv'
+import Ajv, { ErrorObject, ValidateFunction } from 'ajv'
 
-let validate: Ajv.ValidateFunction | undefined
-let ajv: Ajv.Ajv
+let validate: ValidateFunction | undefined
+let ajv: Ajv
 
 interface ValidationResult {
-  result: boolean,
-  errors?: Ajv.ErrorObject[]
+  result: boolean
+  errors?: ErrorObject[]
 }
 
-async function getValidator(): Promise<{ ajv: Ajv.Ajv, validate: Ajv.ValidateFunction }> {  
+async function getValidator(): Promise<{ ajv: Ajv; validate: ValidateFunction }> {
   if (!ajv) {
-    ajv = new Ajv()  
+    ajv = new Ajv()
   }
 
   if (!validate) {
-    const appDataSchema = await import('../schemas/appData.schema.json');
+    const appDataSchema = await import('../schemas/appData.schema.json')
     validate = ajv.compile(appDataSchema)
   }
-  
 
   return { ajv, validate }
 }
 
-export async function validateAppDataDocument(appDataDocument: any): Promise<ValidationResult>{
+export async function validateAppDataDocument(appDataDocument: any): Promise<ValidationResult> {
   const { ajv, validate } = await getValidator()
   const result = !!(await validate(appDataDocument))
 
-
   return {
     result,
-    errors: ajv.errors ?? undefined
+    errors: ajv.errors ?? undefined,
   }
 }
