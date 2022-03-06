c2f370a19b37771c73754450eec57a954e506875
Mati Dastugue <mdastugu@amazon.com>
2022-03-06 12:03:50 -0300

    Add dataDecoder + multihash methods

--- src/utils/appData.spec.ts
+++ src/utils/appData.spec.ts
@@ -1,8 +1,10 @@
-import { validateAppDataDocument } from './appData'
+import { validateAppDataDocument, decodeAppData, decodeMultihash } from './appData'
 
 const VALID_RESULT = {
   result: true,
 }
+
+const INVALID_MULTIHASH_LENGTH = 'multihash length inconsistent'
 
 test('Valid minimal document', async () => {
   const validation = await validateAppDataDocument({
@@ -63,4 +65,31 @@ test('Invalid: No metadata', async () => {
     appCode: 'MyApp',
   })
   expect(validation.result).toBeFalsy()
+})
+
+test('Valid AppData decode', async () => {
+  const appData = await decodeAppData('0xa6c81f4ca727252a05b108f1742a07430f28d474d2a3492d8f325746824d22e5')
+  const validation = await validateAppDataDocument(appData)
+  expect(validation).toEqual(VALID_RESULT)
+})
+
+test('Invalid: AppData decode incorrect hash length', async () => {
+  try {
+    await decodeAppData('0xa6c81f4ca727252a05b108f1742a07430f28d474d2a3492d8f325746824d22e522')
+  } catch (e: any) {
+    expect(e.message).toEqual(expect.stringContaining(INVALID_MULTIHASH_LENGTH))
+  }
+})
+
+test('Valid decode multihash digest', () => {
+  const decodedHash = decodeMultihash('QmUf2TrpSANVXdgcYfAAACe6kg551cY3rAemB7xfEMjYvs')
+  expect(decodedHash).toEqual('0x5ddb2c8207c10b96fac92cb934ef9ba004bc007a073c9e5b13edc422f209ed80')
+})
+
+test('Invalid: multihash incorrect length', () => {
+  try {
+    decodeMultihash('QmUf2TrpSANVXdgcYfAA')
+  } catch (e: any) {
+    expect(e.message).toEqual(expect.stringContaining(INVALID_MULTIHASH_LENGTH))
+  }
 })
--- src/utils/appData.ts
+++ src/utils/appData.ts
@@ -1,4 +1,9 @@
 import Ajv, { ErrorObject, ValidateFunction } from 'ajv'
+import CID from 'cids'
+import fetch from 'cross-fetch'
+import multihashes from 'multihashes'
+import { fromHexString } from './common'
+import { DEFAULT_IPFS_URI } from '../constants'
 
 let validate: ValidateFunction | undefined
 let ajv: Ajv
@@ -21,12 +26,57 @@ async function getValidator(): Promise<{ ajv: Ajv; validate: ValidateFunction }>
   return { ajv, validate }
 }
 
-export async function validateAppDataDocument(appDataDocument: any): Promise<ValidationResult> {
+function buildCidInstance(hash: string) {
+  const cidVersion = 0x1 //.toString(16) //cidv1
+  const codec = 0x70 //.toString(16) //dag-pb
+  const type = 0x12 //.toString(16) //sha2-256
+  const length = 32 //.toString(16) //256 bits
+  const _hash = hash.replace(/(^0x)/, '')
+
+  const hexHash = fromHexString(_hash)
+
+  if (!hexHash) return
+
+  const uint8array = Uint8Array.from([cidVersion, codec, type, length, ...hexHash])
+
+  return new CID(uint8array)
+}
+
+async function loadIpfsFromCid(cid: string) {
+  const response = await fetch(`${DEFAULT_IPFS_URI}/${cid}`)
+
+  return await response.json()
+}
+
+export async function validateAppDataDocument(appDataDocument: unknown): Promise<ValidationResult> {
   const { ajv, validate } = await getValidator()
-  const result = !!(await validate(appDataDocument))
+  const result = !!validate(appDataDocument)
 
   return {
     result,
     errors: ajv.errors ?? undefined,
   }
+}
+
+export async function decodeAppData(hash: string) {
+  const cid = buildCidInstance(hash)
+  if (!cid) return
+  let cidV0
+  try {
+    cidV0 = cid.toV0().toString()
+  } catch (e) {
+    console.error(`Not able to extract CIDv0 from hash '${hash}'`, e)
+    return
+  }
+
+  try {
+    return await loadIpfsFromCid(cidV0)
+  } catch (e) {
+    console.error(`Failed to fetch data from IPFS for CIDv0 ${cidV0} (hash ${hash})`, e)
+  }
+}
+
+export function decodeMultihash(ipfsHash: string): string {
+  const { digest } = multihashes.decode(new CID(ipfsHash).multihash)
+  return `0x${Buffer.from(digest).toString('hex')}`
 }
