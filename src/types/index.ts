08e90a380e4802ed049d0307a88066ce41df55fe
Mati Dastugue <mdastugu@amazon.com>
2022-03-18 17:41:58 -0300

    Update relative paths

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,4 @@
-export * from '../api/cow/types'
+export * from 'api/cow/types'
 export { OrderKind } from '@gnosis.pm/gp-v2-contracts'
 export class Token {
   constructor(public symbol: string, public address: string) {}
