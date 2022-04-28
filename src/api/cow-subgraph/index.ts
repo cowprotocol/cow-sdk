a69c5e6e44017f9e6c23b164a1a093f7b76d9e79
Ramiro Vazquez <ramirovazquez@gmail.com>
2022-04-28 19:12:58 -0300

    fix client not calling the right endpoint when chain id changed

--- src/api/cow-subgraph/index.ts
+++ src/api/cow-subgraph/index.ts
@@ -6,34 +6,34 @@ import { Context } from '../../utils/context'
 import { SupportedChainId as ChainId } from '../../constants/chains'
 import { LastDaysVolumeQuery, LastHoursVolumeQuery, TotalsQuery } from './graphql'
 
-export function getSubgraphUrls(): Record<ChainId, string> {
-  return {
-    [ChainId.MAINNET]: 'https://api.thegraph.com/subgraphs/name/cowprotocol/cow',
-    [ChainId.RINKEBY]: 'https://api.thegraph.com/subgraphs/name/cowprotocol/cow-rinkeby',
-    [ChainId.GNOSIS_CHAIN]: 'https://api.thegraph.com/subgraphs/name/cowprotocol/cow-gc',
-  }
+export const subgraphUrls: Record<ChainId, string> = {
+  [ChainId.MAINNET]: 'https://api.thegraph.com/subgraphs/name/cowprotocol/cow',
+  [ChainId.RINKEBY]: 'https://api.thegraph.com/subgraphs/name/cowprotocol/cow-rinkeby',
+  [ChainId.GNOSIS_CHAIN]: 'https://api.thegraph.com/subgraphs/name/cowprotocol/cow-gc',
 }
 
 export class CowSubgraphApi {
   context: Context
-  client: Promise<GraphQLClient>
+  clients: Record<ChainId, GraphQLClient>
 
   API_NAME = 'CoW Protocol Subgraph'
 
   constructor(context: Context) {
     this.context = context
-    this.client = this.createClient()
+    this.clients = this.createClients()
   }
 
-  private async createClient(): Promise<GraphQLClient> {
-    const baseUrl = await this.getBaseUrl()
-    const client = new GraphQLClient(baseUrl, { fetch: fetch })
-    return client
+  private createClients(): Record<ChainId, GraphQLClient> {
+    return {
+      [ChainId.MAINNET]: new GraphQLClient(subgraphUrls[ChainId.MAINNET], { fetch }),
+      [ChainId.RINKEBY]: new GraphQLClient(subgraphUrls[ChainId.RINKEBY], { fetch }),
+      [ChainId.GNOSIS_CHAIN]: new GraphQLClient(subgraphUrls[ChainId.GNOSIS_CHAIN], { fetch }),
+    }
   }
 
   async getBaseUrl(): Promise<string> {
     const chainId = await this.context.chainId
-    return getSubgraphUrls()[chainId]
+    return subgraphUrls[chainId]
   }
 
   async getTotals(): Promise<TotalsQuery['totals'][0]> {
@@ -87,7 +87,8 @@ export class CowSubgraphApi {
 
   async runQuery<T>(query: string, variables?: Variables): Promise<T> {
     try {
-      const client = await this.client
+      const chainId = await this.context.chainId
+      const client = this.clients[chainId]
       return client.request(query, variables)
     } catch (error) {
       log.error(error)
