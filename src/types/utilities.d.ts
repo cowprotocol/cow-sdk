5da6bde0f4926f31e1431adb403c377e9d2b6987
Ramiro Vazquez <ramirovazquez@gmail.com>
2022-06-08 15:01:13 -0300

    replace any to unknown

--- src/types/utilities.d.ts
+++ src/types/utilities.d.ts
@@ -1,3 +1,5 @@
 type UnionKeys<T> = T extends T ? keyof T : never
-type StrictUnionHelper<T, TAll> = T extends any ? T & Partial<Record<Exclude<UnionKeys<TAll>, keyof T>, never>> : never
+type StrictUnionHelper<T, TAll> = T extends unknown
+  ? T & Partial<Record<Exclude<UnionKeys<TAll>, keyof T>, never>>
+  : never
 export type StrictUnion<T> = StrictUnionHelper<T, T>
