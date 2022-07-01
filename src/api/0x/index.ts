b70898e8e2113e33f3ed765e60bcdd54281221f2
w3stside <david@cow.fi>
2022-07-01 11:23:33 +0100

    adding 0x types
    simplify
    
    logic update
    
    add types

--- src/api/0x/index.ts
+++ src/api/0x/index.ts
@@ -1,25 +1,13 @@
 import { OrderKind } from '@cowprotocol/contracts'
 import { SupportedChainId } from '../../constants/chains'
 import { Context } from '../../utils/context'
+import { getTokensFromMarket } from '../../utils/market'
+import { toErc20Address } from '../../utils/tokens'
 
 import BaseApi from '../base'
 
 import { PriceInformation, PriceQuoteParams } from '../cow/types'
 import { MatchaPriceQuote } from './types'
-
-function getMatchaChainId(chainId: SupportedChainId): SupportedChainId | null {
-  switch (chainId) {
-    // Support: Mainnet, Ropsten, Polygon, Binance Smart Chain
-    // See https://0x.org/docs/api#introduction
-    // but we only support mainnet of that list so..
-    case SupportedChainId.MAINNET:
-      return chainId
-
-    default:
-      // Unsupported network
-      return null
-  }
-}
 
 // Defaults
 const API_NAME = '0x'
@@ -32,14 +20,13 @@ const MATCHA_DEFAULT_OPTIONS = `affiliateAddress=${AFFILIATE_ADDRESS}&excludedSo
 
 export class ZeroXApi extends BaseApi {
   constructor(context: Context) {
-    super({ context, name: API_NAME, baseUrl: _get0xUrls(context.isDevEnvironment, API_URL_VERSION) })
+    super({ context, name: API_NAME, baseUrl: _get0xUrls() })
   }
 
   async getQuote(params: PriceQuoteParams): Promise<MatchaPriceQuote | null> {
+    const { amount, baseToken, quoteToken, kind } = params
     const chainId = await this.context.chainId
-    const { baseToken, quoteToken, amount, kind } = getValidParams(params)
-
-    const networkId = getMatchaChainId(chainId)
+    const networkId = _getMatchaChainId(chainId)
     if (networkId == null) {
       // Unsupported network
       return null
@@ -48,7 +35,11 @@ export class ZeroXApi extends BaseApi {
     console.log(`[pricesApi:${API_NAME}] Get price from ${API_NAME}`, params)
 
     // Buy/sell token and side (sell/buy)
-    const { sellToken, buyToken } = getTokensFromMarket({ baseToken, quoteToken, kind })
+    const { sellToken, buyToken } = getTokensFromMarket({
+      baseToken: toErc20Address(baseToken, chainId),
+      quoteToken: toErc20Address(quoteToken, chainId),
+      kind,
+    })
     const swapSide = kind === OrderKind.BUY ? 'buyAmount' : 'sellAmount'
 
     const response = await this.fetch(
@@ -82,5 +73,19 @@ export function toPriceInformation(priceRaw: MatchaPriceQuote | null, kind: Orde
     return { amount: sellAmount, token: sellTokenAddress }
   } else {
     return { amount: buyAmount, token: buyTokenAddress }
+  }
+}
+
+function _getMatchaChainId(chainId: SupportedChainId): SupportedChainId | null {
+  switch (chainId) {
+    // Support: Mainnet, Ropsten, Polygon, Binance Smart Chain
+    // See https://0x.org/docs/api#introduction
+    // but we only support mainnet of that list so..
+    case SupportedChainId.MAINNET:
+      return chainId
+
+    default:
+      // Unsupported network
+      return null
   }
 }
---
+++ src/api/0x/types.ts
@@ -0,0 +1,100 @@
+import { SupportedChainId } from '../../constants/chains'
+// copy/pasting as the library types correspond to the internal types, not API response
+// e.g "price: BigNumber" when we want the API response type: "price: string"
+// see link below to see
+// https://github.com/0xProject/0x-api/blob/8c4cc7bb8d4fa06a220b7dfd5784361c05daa92a/src/types.ts#L229
+interface GetSwapQuoteResponseLiquiditySource {
+  name: string
+  proportion: string
+  intermediateToken?: string
+  hops?: string[]
+}
+
+// https://github.com/0xProject/0x-api/blob/8c4cc7bb8d4fa06a220b7dfd5784361c05daa92a/src/types.ts#L229
+interface MatchaBaseQuote {
+  chainId: SupportedChainId
+  price: string
+  buyAmount: string
+  sellAmount: string
+  sources: GetSwapQuoteResponseLiquiditySource[]
+  gasPrice: string
+  estimatedGas: string
+  sellTokenToEthRate: string
+  buyTokenToEthRate: string
+  protocolFee: string
+  minimumProtocolFee: string
+  allowanceTarget?: string
+}
+
+// https://github.com/0xProject/0x-api/blob/8c4cc7bb8d4fa06a220b7dfd5784361c05daa92a/src/types.ts#L229
+export interface MatchaPriceQuote extends MatchaBaseQuote {
+  sellTokenAddress: string
+  buyTokenAddress: string
+  value: string
+  gas: string
+}
+
+export type MatchaOptions = {
+  affiliateAddress: string
+  excludedSources: ERC20BridgeSource[]
+}
+
+// Excludable price sources
+// from https://github.com/0xProject/protocol/blob/4f32f3174f25858644eae4c3de59c3a6717a757c/packages/asset-swapper/src/utils/market_operation_utils/types.ts#L38
+export enum ERC20BridgeSource {
+  Native = 'Native',
+  Uniswap = 'Uniswap',
+  UniswapV2 = 'Uniswap_V2',
+  Eth2Dai = 'Eth2Dai',
+  Kyber = 'Kyber',
+  Curve = 'Curve',
+  LiquidityProvider = 'LiquidityProvider',
+  MultiBridge = 'MultiBridge',
+  Balancer = 'Balancer',
+  BalancerV2 = 'Balancer_V2',
+  Cream = 'CREAM',
+  Bancor = 'Bancor',
+  MakerPsm = 'MakerPsm',
+  MStable = 'mStable',
+  Mooniswap = 'Mooniswap',
+  MultiHop = 'MultiHop',
+  Shell = 'Shell',
+  Swerve = 'Swerve',
+  SnowSwap = 'SnowSwap',
+  SushiSwap = 'SushiSwap',
+  Dodo = 'DODO',
+  DodoV2 = 'DODO_V2',
+  CryptoCom = 'CryptoCom',
+  Linkswap = 'Linkswap',
+  KyberDmm = 'KyberDMM',
+  Smoothy = 'Smoothy',
+  Component = 'Component',
+  Saddle = 'Saddle',
+  XSigma = 'xSigma',
+  UniswapV3 = 'Uniswap_V3',
+  CurveV2 = 'Curve_V2',
+  Lido = 'Lido',
+  ShibaSwap = 'ShibaSwap',
+  Clipper = 'Clipper',
+  // BSC only
+  PancakeSwap = 'PancakeSwap',
+  PancakeSwapV2 = 'PancakeSwap_V2',
+  BakerySwap = 'BakerySwap',
+  Nerve = 'Nerve',
+  Belt = 'Belt',
+  Ellipsis = 'Ellipsis',
+  ApeSwap = 'ApeSwap',
+  CafeSwap = 'CafeSwap',
+  CheeseSwap = 'CheeseSwap',
+  JulSwap = 'JulSwap',
+  ACryptos = 'ACryptoS',
+  // Polygon only
+  QuickSwap = 'QuickSwap',
+  ComethSwap = 'ComethSwap',
+  Dfyn = 'Dfyn',
+  WaultSwap = 'WaultSwap',
+  Polydex = 'Polydex',
+  FirebirdOneSwap = 'FirebirdOneSwap',
+  JetSwap = 'JetSwap',
+  IronSwap = 'IronSwap',
+}
