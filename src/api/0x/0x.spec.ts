eff31f681de98d7746ff84f5286c53272dc19991
w3stside <david@cow.fi>
2022-07-12 13:21:07 +0100

    0x error tests

--- src/api/0x/0x.spec.ts
+++ src/api/0x/0x.spec.ts
@@ -97,8 +97,7 @@ test('Valid: Get Price Quote with custom options', async () => {
   expect(price?.value).toEqual(PRICE_QUOTE_RESPONSE.value)
   expect(price?.buyTokenAddress).toEqual(PRICE_QUOTE_RESPONSE.buyTokenAddress)
 })
-
-test('Invalid: Get Price Quote', async () => {
+test('Invalid: Get Price Quote - Insufficient liquidity', async () => {
   fetchMock.mockResponseOnce(
     JSON.stringify({
       code: 100,
@@ -115,4 +114,127 @@ test('Invalid: Get Price Quote', async () => {
   )
 
   await expect(cowSdk.zeroXApi.getQuote(query)).rejects.toThrowError(ZeroXError)
+})
+// normally this won't happen - but just in case we get 400 with no
+// standard error response body
+test('Invalid: Get Price Quote - 400', async () => {
+  fetchMock.mockResponseOnce(JSON.stringify({}), { status: 400 })
+  try {
+    await zeroXSdk.api.getQuote({
+      baseToken: '0x6810e776880c02933d47db1b9fc05908e5386b96',
+      quoteToken: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+      amount: '0',
+      kind: OrderKind.BUY,
+    } as PriceQuoteParams)
+  } catch (e) {
+    const error = e as ZeroXError
+
+    expect(error.message).toEqual(
+      'Price fetch failed. This may be due to a server or network connectivity issue. Please try again later.'
+    )
+    expect(error.error_code).toEqual('400')
+    expect(fetchMock).toHaveBeenCalledTimes(1)
+  }
+})
+test('Invalid: Get Price Quote - 404', async () => {
+  fetchMock.mockResponseOnce(JSON.stringify({}), { status: 404 })
+  try {
+    await zeroXSdk.api.getQuote({
+      baseToken: '0x6810e776880c02933d47db1b9fc05908e5386b96',
+      quoteToken: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+      amount: '0',
+      kind: OrderKind.BUY,
+    } as PriceQuoteParams)
+  } catch (e) {
+    const error = e as ZeroXError
+
+    expect(error.message).toEqual('Not found')
+    expect(error.error_code).toEqual('404')
+    expect(fetchMock).toHaveBeenCalledTimes(1)
+  }
+})
+test('Invalid: Get Price Quote - 429', async () => {
+  fetchMock.mockResponseOnce(JSON.stringify({}), { status: 429 })
+  try {
+    await zeroXSdk.api.getQuote({
+      baseToken: '0x6810e776880c02933d47db1b9fc05908e5386b96',
+      quoteToken: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+      amount: '0',
+      kind: OrderKind.BUY,
+    } as PriceQuoteParams)
+  } catch (e) {
+    const error = e as ZeroXError
+
+    expect(error.message).toEqual('Too many requests - Rate limit exceeded')
+    expect(error.error_code).toEqual('429')
+    expect(fetchMock).toHaveBeenCalledTimes(1)
+  }
+})
+test('Invalid: Get Price Quote - 500', async () => {
+  fetchMock.mockResponseOnce(JSON.stringify({}), { status: 500 })
+  try {
+    await zeroXSdk.api.getQuote({
+      baseToken: '0x6810e776880c02933d47db1b9fc05908e5386b96',
+      quoteToken: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+      amount: '0',
+      kind: OrderKind.BUY,
+    } as PriceQuoteParams)
+  } catch (e) {
+    const error = e as ZeroXError
+
+    expect(error.message).toEqual('Internal server error')
+    expect(error.error_code).toEqual('500')
+    expect(fetchMock).toHaveBeenCalledTimes(1)
+  }
+})
+test('Invalid: Get Price Quote - 501', async () => {
+  fetchMock.mockResponseOnce(JSON.stringify({}), { status: 501 })
+  try {
+    await zeroXSdk.api.getQuote({
+      baseToken: '0x6810e776880c02933d47db1b9fc05908e5386b96',
+      quoteToken: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+      amount: '0',
+      kind: OrderKind.BUY,
+    } as PriceQuoteParams)
+  } catch (e) {
+    const error = e as ZeroXError
+
+    expect(error.message).toEqual('Not Implemented')
+    expect(error.error_code).toEqual('501')
+    expect(fetchMock).toHaveBeenCalledTimes(1)
+  }
+})
+test('Invalid: Get Price Quote - 503', async () => {
+  fetchMock.mockResponseOnce(JSON.stringify({}), { status: 503 })
+  try {
+    await zeroXSdk.api.getQuote({
+      baseToken: '0x6810e776880c02933d47db1b9fc05908e5386b96',
+      quoteToken: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+      amount: '0',
+      kind: OrderKind.BUY,
+    } as PriceQuoteParams)
+  } catch (e) {
+    const error = e as ZeroXError
+
+    expect(error.message).toEqual('Server Error - Too many open connections')
+    expect(error.error_code).toEqual('503')
+    expect(fetchMock).toHaveBeenCalledTimes(1)
+  }
+})
+test('Invalid: Get Price Quote - unknown', async () => {
+  fetchMock.mockResponseOnce(JSON.stringify({}), { status: 600 })
+  try {
+    await zeroXSdk.api.getQuote({
+      baseToken: '0x6810e776880c02933d47db1b9fc05908e5386b96',
+      quoteToken: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2',
+      amount: '0',
+      kind: OrderKind.BUY,
+    } as PriceQuoteParams)
+  } catch (e) {
+    const error = e as ZeroXError
+
+    expect(error.message).toEqual('Error fetching quote')
+    expect(error.error_code).toEqual('600')
+    expect(fetchMock).toHaveBeenCalledTimes(1)
+  }
 })
