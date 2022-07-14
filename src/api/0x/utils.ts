edbff58e1efc008d421361e8f59dbebfa0f82dbf
w3stside <david@cow.fi>
2022-07-14 14:32:09 +0100

    create separate utils file for 0x

--- src/api/0x/utils.ts
+++ src/api/0x/utils.ts
@@ -1,6 +1,18 @@
 import { OrderKind } from '@cowprotocol/contracts'
-import { PriceInformation } from '../cow/types'
-import { MatchaPriceQuote } from './types'
+import log from 'loglevel'
+import { SupportedChainId } from '../../constants/chains'
+import { objectToQueryString, CowError } from '../../utils/common'
+import { PriceInformation, PriceQuoteParams } from '../cow/types'
+import ZeroXError, { ZeroXErrorResponse, logPrefix } from './error'
+import { MatchaOptions, MatchaPriceQuote } from './types'
+
+export function get0xUrls(): Partial<Record<SupportedChainId, string>> {
+  // Support: Mainnet, Ropsten, Polygon, Binance Smart Chain
+  // See https://0x.org/docs/api#introduction
+  return {
+    [SupportedChainId.MAINNET]: 'https://api.0x.org/swap',
+  }
+}
 
 export function normaliseQuoteResponse(priceRaw: MatchaPriceQuote | null, kind: OrderKind): PriceInformation | null {
   if (!priceRaw || !priceRaw.price) {
@@ -14,4 +26,79 @@ export function normaliseQuoteResponse(priceRaw: MatchaPriceQuote | null, kind: 
   } else {
     return { amount: buyAmount, token: buyTokenAddress }
   }
+}
+
+export function getMatchaChainId(chainId: SupportedChainId): SupportedChainId | null {
+  switch (chainId) {
+    // Support: Mainnet, Ropsten, Polygon, Binance Smart Chain
+    // See https://0x.org/docs/api#introduction
+    // but we only support mainnet of that list so..
+    case SupportedChainId.MAINNET:
+      return chainId
+
+    default:
+      // Unsupported network
+      return null
+  }
+}
+
+export function optionsToMatchaParamsUrl({
+  excludedSources,
+  affiliateAddress,
+}: {
+  excludedSources: MatchaOptions['excludedSources']
+  affiliateAddress: string
+}): string {
+  const excludedSourceString = extractExcludedSources({ excludedSources })
+
+  return objectToQueryString({ excludedSources: excludedSourceString, affiliateAddress })
+}
+
+export async function handleQuoteResponse<T = unknown, P extends PriceQuoteParams = PriceQuoteParams>(
+  response: Response,
+  params?: P
+): Promise<T> {
+  if (!response.ok) {
+    const errorObj: ZeroXErrorResponse = await response.clone().json()
+
+    let priceError
+    if (errorObj?.code && errorObj?.reason) {
+      priceError = new ZeroXError(errorObj)
+    } else {
+      const errorMessage = await ZeroXError.getErrorFromStatusCode(response.clone())
+      priceError = new ZeroXError({ code: response.status, reason: errorMessage })
+    }
+
+    if (params) {
+      const { quoteToken, baseToken } = params
+      log.error(logPrefix, `Error querying fee from API - quoteToken: ${quoteToken}, baseToken: ${baseToken}`)
+    }
+
+    throw priceError
+  } else {
+    return response.json()
+  }
+}
+
+export function throwOrReturnAffiliateAddress({
+  affiliateAddressMap,
+  chainId,
+}: {
+  affiliateAddressMap: MatchaOptions['affiliateAddressMap']
+  chainId: SupportedChainId
+}): string {
+  const affiliateAddress = affiliateAddressMap[chainId]
+  if (!affiliateAddress) {
+    throw _getMissingAffiliateAddressError({ affiliateAddressMap, chainId })
+  }
+
+  return affiliateAddress
+}
+
+export function extractExcludedSources({ excludedSources }: Pick<MatchaOptions, 'excludedSources'>) {
+  return excludedSources.length > 0 ? excludedSources.join(',') : ''
+}
+
+function _getMissingAffiliateAddressError(params: Record<string, unknown>): CowError {
+  return new CowError(`Invalid affiliate address! Parameters passed: ${JSON.stringify(params, null, 2)}`)
 }
