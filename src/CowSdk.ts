b4bf43805e6aea7f133034cf1119afedf9ee494c
w3stside <david@cow.fi>
2022-07-22 16:26:31 +0100

    update cowsdk

--- src/CowSdk.ts
+++ src/CowSdk.ts
@@ -7,7 +7,7 @@ import { Context, CowContext } from './utils/context'
 import { signOrder, signOrderCancellation, UnsignedOrder } from './utils/sign'
 import { ZeroXApi } from './api/0x'
 import { MatchaOptions } from './api/0x/types'
-import { ParaswapApi, ParaswapApiStatic } from './api/paraswap'
+import ParaswapApi from './api/paraswap'
 
 type Options = {
   loglevel?: LogLevelDesc
@@ -19,8 +19,8 @@ export class CowSdk<T extends ChainId> {
   cowApi: CowApi
   metadataApi: MetadataApi
   cowSubgraphApi: CowSubgraphApi
-  zeroXApi?: ZeroXApi
-  paraswapApi?: ParaswapApi | ParaswapApiStatic
+  zeroXApi: ZeroXApi
+  paraswapApi: ParaswapApi
 
   constructor(chainId: T = SupportedChainId.MAINNET as T, cowContext: CowContext = {}, options: Options = {}) {
     this.context = new Context(chainId, { ...cowContext })
@@ -28,8 +28,8 @@ export class CowSdk<T extends ChainId> {
     this.cowSubgraphApi = new CowSubgraphApi(this.context)
     this.metadataApi = new MetadataApi(this.context)
     this.zeroXApi = new ZeroXApi(chainId, options.matchaOptions)
-    this.paraswapApi = ParaswapApi.instantiate(chainId)
-    log.setLevel(options.loglevel || 'error')
+    this.paraswapApi = new ParaswapApi()
+    log.setLevel(options.loglevel || 'ERROR')
   }
 
   updateChainId = (chainId: ChainId) => {
