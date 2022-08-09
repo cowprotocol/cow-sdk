5a947aa0f4e65773f71adc8926bc675bd6a93067
Ramiro Vazquez <ramirovazquez@gmail.com>
2022-08-09 14:03:24 -0300

    export missing types

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,6 +1,10 @@
 export * from '../api/cow/types'
+export * from '../api/0x/types'
+export * from '../api/paraswap/types'
 export * from '../api/metadata/types'
 export * from './sdk'
+export * from './utilities'
+
 export { OrderKind } from '@cowprotocol/contracts'
 export class Token {
   constructor(public symbol: string, public address: string) {}
