eeabc740cbd0759d681536371a4241eee8fd488c
Alfetopito <alfetopito@users.noreply.github.com>
2022-08-26 17:34:12 +0100

    Removing Rinkeby definition

--- src/constants/chains.ts
+++ src/constants/chains.ts
@@ -1,13 +1,11 @@
 export enum SupportedChainId {
   MAINNET = 1,
-  RINKEBY = 4,
   GOERLI = 5,
   GNOSIS_CHAIN = 100,
 }
 
 export const ALL_SUPPORTED_CHAIN_IDS: SupportedChainId[] = [
   SupportedChainId.MAINNET,
-  SupportedChainId.RINKEBY,
   SupportedChainId.GOERLI,
   SupportedChainId.GNOSIS_CHAIN,
 ]
