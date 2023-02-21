911f52192fc8d55ff3784a76d2dc108b79559f47
Anxo Rodriguez <anxolin@gmail.com>
2023-02-21 19:11:24 +0000

    Add support to custom subgraph URL

--- src/api/cow-subgraph/index.ts
+++ src/api/cow-subgraph/index.ts
@@ -7,35 +7,47 @@ import { SupportedChainId as ChainId } from '../../constants/chains'
 import { LastDaysVolumeQuery, LastHoursVolumeQuery, TotalsQuery } from './graphql'
 import { LAST_DAYS_VOLUME_QUERY, LAST_HOURS_VOLUME_QUERY, TOTALS_QUERY } from './queries'
 
-export function getSubgraphUrl(env: Env): Partial<Record<ChainId, string>> {
-  switch (env) {
-    case 'staging':
-      return {
-        [ChainId.MAINNET]: 'https://api.thegraph.com/subgraphs/name/cowprotocol/cow-staging',
-        [ChainId.GNOSIS_CHAIN]: 'https://api.thegraph.com/subgraphs/name/cowprotocol/cow-gc-staging',
-      }
-    case 'prod':
-      return {
-        [ChainId.MAINNET]: 'https://api.thegraph.com/subgraphs/name/cowprotocol/cow',
-        [ChainId.GOERLI]: 'https://api.thegraph.com/subgraphs/name/cowprotocol/cow-goerli',
-        [ChainId.GNOSIS_CHAIN]: 'https://api.thegraph.com/subgraphs/name/cowprotocol/cow-gc',
-      }
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export function getDefaultSubgraphUrls(_env: Env): Record<ChainId, string> {
+  // TODO: After evaluating Satsuma.xyz, we should re-create the staging environment
+  // switch (env) {
+  //   case 'staging':
+  //     return {
+  //       [ChainId.MAINNET]: 'https://api.thegraph.com/subgraphs/name/cowprotocol/cow-staging',
+  //       [ChainId.GNOSIS_CHAIN]: 'https://api.thegraph.com/subgraphs/name/cowprotocol/cow-gc-staging',
+  //     }
+  //   case 'prod':
+  //     return {
+  //       [ChainId.MAINNET]: 'https://api.thegraph.com/subgraphs/name/cowprotocol/cow',
+  //       [ChainId.GOERLI]: 'https://api.thegraph.com/subgraphs/name/cowprotocol/cow-goerli',
+  //       [ChainId.GNOSIS_CHAIN]: 'https://api.thegraph.com/subgraphs/name/cowprotocol/cow-gc',
+  //     }
+  // }
+  return {
+    [ChainId.MAINNET]: 'https://subgraph.satsuma-prod.com/94b7bd7c35c5/cow/cow/api',
+    [ChainId.GOERLI]: 'https://subgraph.satsuma-prod.com/94b7bd7c35c5/cow/cow-goerli/api',
+    [ChainId.GNOSIS_CHAIN]: 'https://subgraph.satsuma-prod.com/94b7bd7c35c5/cow/cow-gc/api',
   }
 }
 
 export class CowSubgraphApi {
   context: Context
+  baseUrls: Record<ChainId, string>
 
   API_NAME = 'CoW Protocol Subgraph'
 
   constructor(context: Context) {
     this.context = context
+    this.baseUrls = context.subgraphBaseUrls ?? getDefaultSubgraphUrls(context.env)
   }
 
   async getBaseUrl(options: SubgraphOptions = {}): Promise<string> {
     const { chainId: networkId, env = 'prod' } = options
     const chainId = networkId || (await this.context.chainId)
-    const baseUrl = getSubgraphUrl(env)[chainId]
+
+    this.context.chainId
+
+    const baseUrl = this.baseUrls[chainId]
     if (!baseUrl) {
       throw new CowError(`No network support for SubGraph in ChainId ${networkId} and Environment "${env}"`)
     }
