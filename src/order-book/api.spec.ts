5c76eed358ec9619fbb2039422136839701fe0cb
Alexandr Kazachenko <shoom3301@gmail.com>
2023-03-31 17:45:46 +0600

    Fix tests

--- src/order-book/api.spec.ts
+++ src/order-book/api.spec.ts
@@ -1,3 +1,15 @@
+jest.mock('cross-fetch', () => {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  const fetchMock = require('jest-fetch-mock')
+  // Require the original module to not be mocked...
+  const originalFetch = jest.requireActual('cross-fetch')
+  return {
+    __esModule: true,
+    ...originalFetch,
+    default: fetchMock,
+  }
+})
+
 import fetchMock, { enableFetchMocks } from 'jest-fetch-mock'
 import { CowError } from '../common/cow-error'
 import { OrderBookApi } from './api'
