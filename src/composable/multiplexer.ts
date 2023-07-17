0d3fa3ac766aac6f6ba4f4c26944d1f8033704fc
mfw78 <mfw78@protonmail.com>
2023-07-17 14:01:57 +0000

    feat(composable): remaining multiplexer feature set

--- src/composable/multiplexer.ts
+++ src/composable/multiplexer.ts
@@ -1,15 +1,13 @@
 import { StandardMerkleTree } from '@openzeppelin/merkle-tree'
-import { BigNumber, utils } from 'ethers'
+import { BigNumber, providers, utils } from 'ethers'
 
-import { SupportedChainId } from '../common'
+import { COMPOSABLE_COW_CONTRACT_ADDRESS, SupportedChainId } from '../common'
 import { BaseConditionalOrder, ConditionalOrderParams } from './conditionalorder'
-import assert from 'assert'
+
+import { ComposableCoW__factory } from './generated'
+import { GPv2Order } from './generated/ComposableCoW'
 
 const CONDITIONAL_ORDER_LEAF_ABI = ['address', 'bytes32', 'bytes']
-
-// const PAYLOAD_ABI = [
-//   'tuple(bytes32[] proof, tuple(address handler, bytes32 salt, bytes staticInput) params, bytes offchainInput)',
-// ]
 
 const PAYLOAD_EMITTED_ABI = ['tuple(bytes32[] proof, tuple(address handler, bytes32 salt, bytes staticInput) params)[]']
 
@@ -88,14 +86,13 @@ export type ProofWithParams = {
  * - Generate a merkle tree of conditional orders
  * - Generate proofs for all orders in the merkle tree
  * - Save proofs, with the ability to omit / skip specific conditional orders
- * - Propose `create` and `setRoot` to a ComposableCoW-enabled Safe
- * - BONUS: Upload proofs to IPFS or Swarm and use the location within the ProofStruct to direct indexers.
+ * - Support for passing an optional upload function to upload the proofs to a decentralized storage network
  */
 export class Multiplexer {
   static orderTypeRegistry: Record<string, new (...args: any[]) => BaseConditionalOrder<any, any>> = {}
 
   public chain: SupportedChainId
-  public location?: ProofLocation
+  public location: ProofLocation
 
   private orders: Record<string, BaseConditionalOrder<any, any>> = {}
   private tree?: StandardMerkleTree<string[]>
@@ -105,9 +102,16 @@ export class Multiplexer {
    * @param chain The `chainId` for where we're using `ComposableCoW`.
    * @param orders An optional array of conditional orders to initialize the merkle tree with.
    * @param root An optional root to verify against.
+   * @param location The location of the proofs for the conditional orders.
    */
-  constructor(chain: SupportedChainId, orders?: Record<string, BaseConditionalOrder<any, any>>, root?: string) {
+  constructor(
+    chain: SupportedChainId,
+    orders?: Record<string, BaseConditionalOrder<any, any>>,
+    root?: string,
+    location: ProofLocation = ProofLocation.PRIVATE
+  ) {
     this.chain = chain
+    this.location = location
 
     // If orders are provided, the length must be > 0
     if (orders && Object.keys(orders).length === 0) {
@@ -125,22 +129,15 @@ export class Multiplexer {
     if (orders) {
       this.orders = orders
       this.generate()
-      assert(this.tree, 'Merkle tree not generated')
 
       // if generate was successful, this.tree will be defined and we can verify the root
-      if (this.tree.root !== root) {
+      if (this.tree && this.tree.root !== root) {
         throw new Error('Root mismatch')
       }
     }
   }
 
-  /**
-   * Mark a single conditional order as valid.
-   * @param order The conditional order for `ComposableCoW` to signal it's intent to trade.
-   */
-  static create<T, P>(order: BaseConditionalOrder<T, P>): Promise<string> {
-    throw new Error('Not implemented')
-  }
+  // --- user facing serialization methods ---
 
   /**
    * Given a serialized multiplexer, create the multiplexer and rehydrate all conditional orders.
@@ -204,7 +201,9 @@ export class Multiplexer {
     // Make sure the merkle tree is generated
     if (!this.tree) {
       this.generate()
-      assert(this.tree, 'Merkle tree not generated')
+
+      // If the merkle tree is still not generated, throw
+      if (!this.tree) throw new Error('Merkle tree not generated')
     }
 
     // serialize the multiplexer, including the root but excluding the merkle tree.
@@ -222,6 +221,8 @@ export class Multiplexer {
       return v
     })
   }
+
+  // --- crud methods ---
 
   /**
    * Add a conditional order to the merkle tree.
@@ -259,6 +260,8 @@ export class Multiplexer {
     this.orders[order.id] = order
     this.reset()
   }
+
+  // --- accessors ---
 
   /**
    * Accessor for a given conditional order in the multiplexer.
@@ -298,52 +301,115 @@ export class Multiplexer {
     )
   }
 
+  // --- serialization for watchtowers / indexers ---
+
   /**
-   * Encode the proofs and their parameters for emitting from ComposableCoW.
-   * @param filter {@link getProofs}
-   * @returns An ABI-encoded payload to be used in the `Proof` struct.
+   * The primary method for watch towers to use when deserializing the proofs and parameters for the conditional orders.
+   * @param s The serialized proofs with parameters for consumption by watchtowers / indexers.
+   * @returns The `ProofWithParams` array.
+   * @throws If the `ProofWithParams` array cannot be deserialized.
    */
-  encodeProofAndParamsToEmit(filter?: (v: string[]) => boolean): string {
-    return utils.defaultAbiCoder.encode(
-      PAYLOAD_EMITTED_ABI,
-      this.getProofs(filter).map((p) => ({ proof: p.proof, params: p.params }))
-    )
+  static decodeFromJSON(s: string): ProofWithParams[] {
+    return JSON.parse(s, (_k, v) => {
+      // Make sure we deserialize `BigNumber` correctly
+      if (typeof v === 'object' && v !== null && v.hasOwnProperty('type') && v.hasOwnProperty('hex')) {
+        if (v.type === 'BigNumber') {
+          return BigNumber.from(v)
+        }
+      }
+
+      return v
+    })
   }
 
-  encodeProofStruct(filter?: (v: string[]) => boolean): string {
+  /**
+   * The primary entry point for dapps integrating with `ComposableCoW` to generate the proofs and
+   * parameters for the conditional orders.
+   *
+   * After populating the multiplexer with conditional orders, this method can be used to generate
+   * the proofs and parameters for the conditional orders. The returned `ProofStruct` can then be
+   * used with `setRoot` or `setRootWithContext` on a `ComposableCoW`-enabled Safe.
+   *
+   * @param filter {@link getProofs}
+   * @parma locFn A function that takes the off-chain encoded input, and returns the `location`
+   *        for the `ProofStruct`, and the `data` for the `ProofStruct`.
+   * @returns The ABI-encoded `ProofStruct` for `setRoot` and `setRootWithContext`.
+   */
+  async prepareProofStruct(
+    location: ProofLocation,
+    filter?: (v: string[]) => boolean,
+    uploader?: (offChainEncoded: string) => Promise<string>
+  ): Promise<string> {
     if (!this.tree) {
       throw new Error('Merkle tree not generated')
     }
 
-    // default to private location
-    const location = this.location || ProofLocation.PRIVATE
-
-    const data = (): string => {
+    const data = async (): Promise<string> => {
       switch (location) {
         case ProofLocation.PRIVATE:
           return '0x'
         case ProofLocation.EMITTED:
-          return this.encodeProofAndParamsToEmit(filter)
+          return this.encodeToABI(filter)
         case ProofLocation.SWARM:
-          // TODO: upload to swarm and return the `bmt` root hash. This can be
-          // pre-computed using bee-js.
-          throw new Error('Not implemented')
         case ProofLocation.WAKU:
-          // TODO: define WAKU2-STORE format and return the WAKU2-STORE address
-          // eg. This may need to contain an `enrtree` of the Waku fleet to use,
-          // as well as the `contentTopic` on which to retrieve from.
-          throw new Error('Not implemented')
-        case ProofLocation.RESERVED:
-          throw new Error('Not implemented')
         case ProofLocation.IPFS:
-          // TODO: upload to IPFS and return the IPFS hash
-          throw new Error('Not implemented')
+          if (!uploader) throw new Error('Must provide an uploader function')
+          try {
+            return await uploader(this.encodeToJSON(filter))
+          } catch (e) {
+            throw new Error(`Error uploading to decentralized storage ${location}: ${e}`)
+          }
         default:
           throw new Error('Unsupported location')
       }
     }
 
-    return utils.defaultAbiCoder.encode(PROOF_ABI, [location, data()])
+    return await data()
+      .then((d) => {
+        // if we get here, we have a valid `data` field for the `ProofStruct`
+        // This means that if there was an upload function, it was called and the upload was successful
+        this.location = location
+        return utils.defaultAbiCoder.encode(PROOF_ABI, [location, d])
+      })
+      .catch((e) => {
+        throw new Error(`Error preparing proof struct: ${e}`)
+      })
+  }
+
+  /**
+   * Poll a conditional order to see if it is tradeable.
+   * @param owner The owner of the conditional order.
+   * @param p The proof and parameters.
+   * @param chain Which chain to use for the ComposableCoW contract.
+   * @param provider An RPC provider for the chain.
+   * @param offChainInputFn A function, if provided, that will return the off-chain input for the conditional order.
+   * @throws If the conditional order is not tradeable.
+   * @returns The tradeable `GPv2Order.Data` struct and the `signature` for the conditional order.
+   */
+  static async poll(
+    owner: string,
+    p: ProofWithParams,
+    chain: SupportedChainId,
+    provider: providers.Provider,
+    offChainInputFn?: (owner: string, params: ConditionalOrderParams) => Promise<string>
+  ): Promise<[GPv2Order.DataStructOutput, string]> {
+    const contract = ComposableCoW__factory.connect(COMPOSABLE_COW_CONTRACT_ADDRESS[chain], provider)
+    const offChainInput = offChainInputFn ? await offChainInputFn(owner, p.params) : '0x'
+    return await contract.getTradeableOrderWithSignature(owner, p.params, offChainInput, p.proof)
+  }
+
+  /**
+   * The primary entry point for dumping the proofs and parameters for the conditional orders.
+   *
+   * This is to be used by watchtowers / indexers to store the proofs and parameters for the
+   * conditional orders off-chain. The encoding returned by this method may **NOT** contain all
+   * proofs and parameters, depending on the `filter` provided, and therefore should not be used
+   * to rehydrate the multiplexer from a user's perspective.
+   * @param filter {@link getProofs}
+   * @returns A JSON-encoded string of the proofs and parameters for the conditional orders.
+   */
+  dumpProofs(filter?: (v: string[]) => boolean): string {
+    return this.encodeToJSON(filter)
   }
 
   /**
@@ -353,7 +419,7 @@ export class Multiplexer {
    * @returns An array of proofs and their order's parameters for the conditional orders in the
    *          merkle tree.
    */
-  getProofs(filter?: (v: string[]) => boolean): ProofWithParams[] {
+  private getProofs(filter?: (v: string[]) => boolean): ProofWithParams[] {
     if (!this.tree) {
       throw new Error('Merkle tree not generated')
     }
@@ -387,12 +453,37 @@ export class Multiplexer {
   }
 
   /**
+   * ABI-encode the proofs and parameters for the conditional orders in the merkle tree.
+   * @param filter {@link getProofs}
+   * @returns ABI-encoded `data` for the `ProofStruct`.
+   */
+  private encodeToABI(filter?: (v: string[]) => boolean): string {
+    return utils.defaultAbiCoder.encode(PAYLOAD_EMITTED_ABI, this.getProofs(filter))
+  }
+
+  /**
+   * JSON-encode the proofs and parameters for the conditional orders in the merkle tree.
+   * @param filter {@link getProofs}
+   * @returns The JSON-encoded data for storage off-chain.
+   */
+  private encodeToJSON(filter?: (v: string[]) => boolean): string {
+    return JSON.stringify(this.getProofs(filter))
+  }
+
+  /**
    * A helper to reset the merkle tree.
    */
   private reset(): void {
     this.tree = undefined
   }
 
+  /**
+   * Register a conditional order type with the multiplexer.
+   *
+   * **CAUTION**: This is required for using `Multiplexer.fromJSON` and `Multiplexer.toJSON`.
+   * @param orderType The order type to register.
+   * @param conditionalOrderClass The class to use for the given order type.
+   */
   public static registerOrderType(
     orderType: string,
     conditionalOrderClass: new (...args: any[]) => BaseConditionalOrder<any, any>
