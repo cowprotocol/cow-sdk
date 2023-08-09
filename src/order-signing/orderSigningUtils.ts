a1c470978f0b1c0ddbb4a4d338032fb5ef35cdf2
mfw78 <mfw78@protonmail.com>
2023-08-09 09:47:56 +0000

    chore(es6): no async import

--- src/order-signing/orderSigningUtils.ts
+++ src/order-signing/orderSigningUtils.ts
@@ -3,7 +3,7 @@ import type { Signer } from '@ethersproject/abstract-signer'
 import type { TypedDataDomain } from '@cowprotocol/contracts'
 import type { SigningResult, UnsignedOrder } from './types'
 import { _TypedDataEncoder } from 'ethers/lib/utils'
-const getSignUtils = () => import('./utils')
+import { getDomain, signOrder, signOrderCancellation, signOrderCancellations } from './utils'
 
 /**
  * Utility class for signing order intents and cancellations.
@@ -52,7 +52,6 @@ export class OrderSigningUtils {
    * @returns {Promise<SigningResult>} Encoded signature including signing scheme for the order.
    */
   static async signOrder(order: UnsignedOrder, chainId: SupportedChainId, signer: Signer): Promise<SigningResult> {
-    const { signOrder } = await getSignUtils()
     return signOrder(order, chainId, signer)
   }
 
@@ -68,7 +67,6 @@ export class OrderSigningUtils {
     chainId: SupportedChainId,
     signer: Signer
   ): Promise<SigningResult> {
-    const { signOrderCancellation } = await getSignUtils()
     return signOrderCancellation(orderUid, chainId, signer)
   }
 
@@ -84,7 +82,6 @@ export class OrderSigningUtils {
     chainId: SupportedChainId,
     signer: Signer
   ): Promise<SigningResult> {
-    const { signOrderCancellations } = await getSignUtils()
     return signOrderCancellations(orderUids, chainId, signer)
   }
 
@@ -95,7 +92,6 @@ export class OrderSigningUtils {
    * @see https://eips.ethereum.org/EIPS/eip-712
    */
   static async getDomain(chainId: SupportedChainId): Promise<TypedDataDomain> {
-    const { getDomain } = await getSignUtils()
     return getDomain(chainId)
   }
 
