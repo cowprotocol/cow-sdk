7c71150652ec16c047d6be2f4d86402c3703635a
Alexandr Kazachenko <shoom3301@gmail.com>
2024-01-05 15:05:28 +0600

    feat: util to map supported networks (#194)

--- src/common/consts.ts
+++ src/common/consts.ts
@@ -17,14 +17,20 @@ export const ALL_SUPPORTED_CHAIN_IDS: SupportedChainId[] = [
   SupportedChainId.SEPOLIA,
 ]
 
-export function mapAddressToSupportedNetworks(address: string): Record<SupportedChainId, string> {
-  return ALL_SUPPORTED_CHAIN_IDS.reduce<Record<number, string>>(
+export function mapSupportedNetworks<T>(value: (chainId: SupportedChainId) => T): Record<SupportedChainId, T>
+export function mapSupportedNetworks<T>(value: T): Record<SupportedChainId, T>
+export function mapSupportedNetworks<T>(value: T | ((chainId: SupportedChainId) => T)): Record<SupportedChainId, T> {
+  return ALL_SUPPORTED_CHAIN_IDS.reduce<Record<number, T>>(
     (acc, chainId) => ({
       ...acc,
-      [chainId]: address,
+      [chainId]: typeof value === 'function' ? (value as (chainId: SupportedChainId) => T)(chainId) : value,
     }),
     {}
   )
+}
+
+export function mapAddressToSupportedNetworks(address: string): Record<SupportedChainId, string> {
+  return mapSupportedNetworks(address)
 }
 
 /**
