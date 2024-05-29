143c42b512027a673b725fc6f88c336f9f9c4105
shoom3301 <shoom3301@gmail.com>
2024-05-29 20:29:22 +0500

    feat: utility to calculate quote amounts and costs

---
+++ src/order-book/quoteAmountsAndCostsUtils.ts
@@ -0,0 +1,125 @@
+import { QuoteAmountsAndCosts } from './types'
+import { OrderKind, type OrderParameters } from './generated'
+
+interface Params {
+  orderParams: OrderParameters
+  sellDecimals: number
+  buyDecimals: number
+  slippagePercentBps: number
+  partnerFeeBps: number | undefined
+}
+
+export function getQuoteAmountsAndCosts(params: Params): QuoteAmountsAndCosts {
+  const { orderParams, sellDecimals, buyDecimals, slippagePercentBps } = params
+  const partnerFeeBps = params.partnerFeeBps ?? 0
+  const isSell = orderParams.kind === OrderKind.SELL
+  /**
+   * Wrap raw values into CurrencyAmount objects
+   * We also make amounts names more specific with "beforeNetworkCosts" and "afterNetworkCosts" suffixes
+   */
+  const networkCostAmount = getBigNumber(orderParams.feeAmount, sellDecimals)
+  const sellAmountBeforeNetworkCosts = getBigNumber(orderParams.sellAmount, sellDecimals)
+  const buyAmountAfterNetworkCosts = getBigNumber(orderParams.buyAmount, buyDecimals)
+
+  /**
+   * This is an actual price of the quote since it's derrived only from the quote sell and buy amounts
+   */
+  const quotePrice = buyAmountAfterNetworkCosts.num / sellAmountBeforeNetworkCosts.num
+
+  /**
+   * Before networkCosts + networkCosts = After networkCosts :)
+   */
+  const sellAmountAfterNetworkCosts = getBigNumber(
+    sellAmountBeforeNetworkCosts.big + networkCostAmount.big,
+    sellDecimals
+  )
+
+  /**
+   * Since the quote contains only buy amount after network costs
+   * we have to calculate the buy amount before network costs from the quote price
+   */
+  const buyAmountBeforeNetworkCosts = getBigNumber(quotePrice * sellAmountAfterNetworkCosts.num, buyDecimals)
+
+  /**
+   * Partner fee is always added on the surplus amount, for sell-orders it's buy amount, for buy-orders it's sell amount
+   */
+  const surplusAmount = isSell ? buyAmountBeforeNetworkCosts.big : sellAmountBeforeNetworkCosts.big
+  const partnerFeeAmount = partnerFeeBps > 0 ? surplusAmount / BigInt(partnerFeeBps) : BigInt(0)
+
+  /**
+   * Partner fee is always added on the surplus token, for sell-orders it's buy token, for buy-orders it's sell token
+   */
+  const afterPartnerFees = isSell
+    ? {
+        sellAmount: sellAmountAfterNetworkCosts.big,
+        buyAmount: buyAmountAfterNetworkCosts.big - partnerFeeAmount,
+      }
+    : {
+        sellAmount: sellAmountAfterNetworkCosts.big + partnerFeeAmount,
+        buyAmount: buyAmountAfterNetworkCosts.big,
+      }
+
+  const getSlippageAmount = (amount: bigint) => (amount * BigInt(slippagePercentBps)) / BigInt(100 * 100)
+
+  /**
+   * Same rules apply for slippage as for partner fees
+   */
+  const afterSlippage = isSell
+    ? {
+        sellAmount: afterPartnerFees.sellAmount,
+        buyAmount: afterPartnerFees.buyAmount - getSlippageAmount(afterPartnerFees.buyAmount),
+      }
+    : {
+        sellAmount: afterPartnerFees.sellAmount + getSlippageAmount(afterPartnerFees.sellAmount),
+        buyAmount: afterPartnerFees.buyAmount,
+      }
+
+  return {
+    isSell,
+    quotePrice,
+    costs: {
+      networkFee: {
+        amountInSellCurrency: networkCostAmount.big,
+        amountInBuyCurrency: getBigNumber(quotePrice * networkCostAmount.num, buyDecimals).big,
+      },
+      partnerFee: {
+        amount: partnerFeeAmount,
+        bps: partnerFeeBps,
+      },
+    },
+    beforeNetworkCosts: {
+      sellAmount: sellAmountBeforeNetworkCosts.big,
+      buyAmount: buyAmountBeforeNetworkCosts.big,
+    },
+    afterNetworkCosts: {
+      sellAmount: sellAmountAfterNetworkCosts.big,
+      buyAmount: buyAmountAfterNetworkCosts.big,
+    },
+    afterPartnerFees,
+    afterSlippage,
+  }
+}
+
+type BigNumber = {
+  big: bigint
+  num: number
+}
+
+/**
+ * BigInt works well with subtraction and addition, but it's not very good with multiplication and division
+ * To multiply/divide token amounts we have to convert them to numbers, but we have to be careful with precision
+ * @param value
+ * @param decimals
+ */
+function getBigNumber(value: string | bigint | number, decimals: number): BigNumber {
+  if (typeof value === 'number') {
+    const big = BigInt((value * 10 ** decimals).toFixed())
+
+    return { big, num: value }
+  }
+
+  const big = BigInt(value)
+  const num = Number(big) / 10 ** decimals
+
+  return { big, num }
+}
--- src/order-book/types.ts
+++ src/order-book/types.ts
@@ -6,3 +6,41 @@ import { Order } from './generated'
 export interface EnrichedOrder extends Order {
   totalFee: string
 }
+
+/**
+ * CoW Protocol quote has amounts (sell/buy) and costs (network fee), there is also partner fees.
+ * Besides that, CoW Protocol supports both sell and buy orders and the fees and costs are calculated differently.
+ *
+ * The order of adding fees and costs is as follows:
+ * 1. Network fee is always added to the sell amount
+ * 2. Partner fee is added to the surplus amount (sell amount for sell-orders, buy amount for buy-orders)
+ *
+ * For sell-orders the partner fee is subtracted from the buy amount after network costs.
+ * For buy-orders the partner fee is added on top of the sell amount after network costs.
+ */
+export interface QuoteAmountsAndCosts<
+  Amounts = {
+    sellAmount: bigint
+    buyAmount: bigint
+  }
+> {
+  isSell: boolean
+
+  quotePrice: number
+
+  costs: {
+    networkFee: {
+      amountInSellCurrency: bigint
+      amountInBuyCurrency: bigint
+    }
+    partnerFee: {
+      amount: bigint
+      bps: number
+    }
+  }
+
+  beforeNetworkCosts: Amounts
+  afterNetworkCosts: Amounts
+  afterPartnerFees: Amounts
+  afterSlippage: Amounts
+}
