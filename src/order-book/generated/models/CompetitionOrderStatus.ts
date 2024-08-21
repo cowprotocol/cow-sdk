b898ea3c06a55faea4f5be65efb8e2dd7096fc48
Leandro <alfetopito@users.noreply.github.com>
2024-08-21 14:52:15 +0100

    fix: update orderbook open api (#217)
    * fix: update to fixed orderbook openapi version
    
    * chore: bump version to 5.4.1-RC.0
    
    * chore: pin latest services version
    
    * chore: bump version to 5.4.1

--- src/order-book/generated/models/CompetitionOrderStatus.ts
+++ src/order-book/generated/models/CompetitionOrderStatus.ts
@@ -7,7 +7,8 @@ import type { ExecutedAmounts } from './ExecutedAmounts';
 export type CompetitionOrderStatus = {
     type: CompetitionOrderStatus.type;
     /**
-     * A list of solvers who participated in the latest competition. The presence of executed amounts defines whether the solver provided a solution for the desired order.
+     * A list of solvers who participated in the latest competition, sorted by score in ascending order, where the last element is the winner.
+     * The presence of executed amounts defines whether the solver provided a solution for the desired order.
      *
      */
     value?: Array<{
@@ -22,13 +23,13 @@ export type CompetitionOrderStatus = {
 export namespace CompetitionOrderStatus {
 
     export enum type {
-        OPEN = 'Open',
-        SCHEDULED = 'Scheduled',
-        ACTIVE = 'Active',
-        SOLVED = 'Solved',
-        EXECUTING = 'Executing',
-        TRADED = 'Traded',
-        CANCELLED = 'Cancelled',
+        OPEN = 'open',
+        SCHEDULED = 'scheduled',
+        ACTIVE = 'active',
+        SOLVED = 'solved',
+        EXECUTING = 'executing',
+        TRADED = 'traded',
+        CANCELLED = 'cancelled',
     }
 
 
--- src/order-book/generated/models/PriceImprovement.ts
+++ src/order-book/generated/models/PriceImprovement.ts
@@ -2,11 +2,17 @@
 /* tslint:disable */
 /* eslint-disable */
 
+import type { Quote } from './Quote';
+
 /**
  * The protocol fee is taken as a percent of the order price improvement which is a difference between the executed price and the best quote.
  */
 export type PriceImprovement = {
     factor: number;
     maxVolumeFactor: number;
+    /**
+     * The best quote received.
+     */
+    quote: Quote;
 };
 
