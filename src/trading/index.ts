4f788edbee6f483a27617b9c0d9b084c5d999668
shoom3301 <shoom3301@gmail.com>
2024-11-08 17:53:50 +0500

    chore: export helpers

--- src/trading/index.ts
+++ src/trading/index.ts
@@ -1,9 +1,19 @@
 export * from './types'
 export * from './tradingSdk'
 
+/**
+ * Main trading functions
+ */
 export { getQuote } from './getQuote'
 export { postSwapOrder, postSwapOrderFromQuote } from './postSwapOrder'
 export { postLimitOrder } from './postLimitOrder'
 export { postCoWProtocolTrade } from './postCoWProtocolTrade'
 export { getOrderToSign } from './getOrderToSign'
 export { postOnChainTrade } from './postOnChainTrade'
+
+/**
+ * Helpers
+ */
+
+export * from './appDataUtils'
+export * from './calculateUniqueOrderId'
