1271cf2b93b8518051a0fa69ddfd56c487bef628
shoom3301 <shoom3301@gmail.com>
2025-01-30 12:16:07 +0500

    chore: const -> function

--- src/trading/consts.ts
+++ src/trading/consts.ts
@@ -1,6 +1,6 @@
 import { EcdsaSigningScheme, SigningScheme } from '../order-book'
 
-export const log = (text: string) => {
+export function log(text: string) {
   if (!log.enabled) return
   console.log(`[COW TRADING SDK] ${text}`)
 }
