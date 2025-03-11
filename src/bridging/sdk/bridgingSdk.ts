f3d090f50de7e6d3bd6534c28bcfbef534804059
Anxo Rodriguez <anxolin@gmail.com>
2025-03-11 17:03:42 +0000

    chore: remove some lints

--- src/bridging/sdk/bridgingSdk.ts
+++ src/bridging/sdk/bridgingSdk.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { QuoteAndPost, TraderParameters } from '../../trading'
 import { BridgeProvider, BridgeQuoteResult, GetBuyTokensParams, QuoteBridgeRequest } from '../types'
 import { ALL_SUPPORTED_CHAINS, ChainInfo, TokenInfo } from '../../common'
