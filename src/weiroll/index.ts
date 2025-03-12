1500f7772922b4ffbbf7bd86b344571e2b5fc097
Anxo Rodriguez <anxolin@gmail.com>
2025-03-12 21:13:43 +0000

    feat: add weiroll utils

---
+++ src/weiroll/index.ts
@@ -0,0 +1,74 @@
+import { Planner } from '@weiroll/weiroll.js'
+import { ethers } from 'ethers'
+import { EvmCall } from '../common'
+export type { Contract as WeirollContract } from '@weiroll/weiroll.js'
+
+export enum CommandFlags {
+  DELEGATECALL = 0,
+  CALL = 1,
+  STATICCALL = 2,
+  CALL_WITH_VALUE = 3,
+  CALLTYPE_MASK = 3,
+  EXTENDED_COMMAND = 64,
+  TUPLE_RETURN = 128,
+}
+
+const WEIROLL_ADDRESS = '0x9585c3062Df1C247d5E373Cfca9167F7dC2b5963'
+
+const WEIROLL_ABI = [
+  {
+    inputs: [
+      { internalType: 'uint256', name: 'command_index', type: 'uint256' },
+      { internalType: 'address', name: 'target', type: 'address' },
+      { internalType: 'string', name: 'message', type: 'string' },
+    ],
+    name: 'ExecutionFailed',
+    type: 'error',
+  },
+  {
+    inputs: [
+      { internalType: 'bytes32[]', name: 'commands', type: 'bytes32[]' },
+      { internalType: 'bytes[]', name: 'state', type: 'bytes[]' },
+    ],
+    name: 'execute',
+    outputs: [{ internalType: 'bytes[]', name: '', type: 'bytes[]' }],
+    stateMutability: 'payable',
+    type: 'function',
+  },
+]
+
+function getWeirollCalldata(planner: Planner) {
+  const planResult = planner.plan()
+
+  if (!planResult) {
+    throw new Error('Empty plan')
+  }
+
+  const { commands, state } = planResult
+
+  const weirollInterface = new ethers.utils.Interface(WEIROLL_ABI)
+
+  return weirollInterface.encodeFunctionData('execute', [commands, state])
+}
+
+/**
+ * Create a weiroll transaction
+ *
+ * @param addToPlanner - A function that adds the commands to the planner
+ *
+ * @returns An EVM call
+ */
+export function createWeirollDelegateCall(addToPlanner: (planner: Planner) => void): EvmCall {
+  // Create a new planner
+  const planner = new Planner()
+
+  // Add the commands to the planner
+  addToPlanner(planner)
+
+  // Get the EVM call
+  return {
+    to: WEIROLL_ADDRESS,
+    value: BigInt(0),
+    data: getWeirollCalldata(planner),
+  }
+}
