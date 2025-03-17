835eb94ef73c07d55e2f155d25801ed9400876ec
Anxo Rodriguez <anxolin@gmail.com>
2025-03-17 06:54:40 +0000

    feat: implement API

--- src/bridging/providers/across/AcrossBridgeProvider.ts
+++ src/bridging/providers/across/AcrossBridgeProvider.ts
@@ -14,9 +14,9 @@ import {
 
 import { RAW_PROVIDERS_FILES_PATH } from '../../const'
 
-import { ChainId, ChainInfo, SupportedChainId, TargetChainId } from '../../../chains'
+import { ChainId, ChainInfo, TargetChainId } from '../../../chains'
 
-import { AcrossChainConfig, acrossTokenMapping } from './const/tokens'
+import { acrossTokenMapping } from './const/tokens'
 import { EvmCall, TokenInfo } from '../../../common'
 
 import { mainnet } from '../../../chains/details/mainnet'
@@ -24,14 +24,24 @@ import { polygon } from '../../../chains/details/polygon'
 import { arbitrumOne } from 'src/chains/details/arbitrum'
 import { base } from '../../../chains/details/base'
 import { optimism } from '../../../chains/details/optimism'
+import { AcrossApi, SuggestedFeesResponse } from './acrossApi'
+import { getChainConfigs, getTokenAddress, getTokenSymbol, toBridgeQuoteResult } from './util'
 
 interface AcrossBridgeProviderOptions {
   getTokenInfos: (chainId: ChainId, addresses: string[]) => Promise<TokenInfo[]>
   apiBaseUrl?: string
 }
 
-export class AcrossBridgeProvider implements BridgeProvider<BridgeQuoteResult> {
-  constructor(private options: AcrossBridgeProviderOptions) {}
+export interface AcrossQuoteResult extends BridgeQuoteResult {
+  suggestedFees: SuggestedFeesResponse
+}
+
+export class AcrossBridgeProvider implements BridgeProvider<AcrossQuoteResult> {
+  private api: AcrossApi
+
+  constructor(private options: AcrossBridgeProviderOptions) {
+    this.api = new AcrossApi(options.apiBaseUrl)
+  }
 
   info: BridgeProviderInfo = {
     name: 'Across',
@@ -55,6 +65,7 @@ export class AcrossBridgeProvider implements BridgeProvider<BridgeQuoteResult> {
   }
 
   async getIntermediateTokens(request: QuoteBridgeRequest): Promise<string[]> {
+    // TODO: This is a temporary implementation. We should use the Across API to get the intermediate tokens (see this.getAvailableRoutes())
     const { sellTokenChainId, buyTokenChainId, buyTokenAddress } = request
     const chainConfigs = getChainConfigs(sellTokenChainId, buyTokenChainId)
     if (!chainConfigs) return []
@@ -70,13 +81,26 @@ export class AcrossBridgeProvider implements BridgeProvider<BridgeQuoteResult> {
     return intermediateToken ? [intermediateToken] : []
   }
 
-  async getQuote(_request: QuoteBridgeRequest): Promise<BridgeQuoteResult> {
-    return {
-      feeBps: 10,
-      slippageBps: 0,
-      buyAmount: '123456',
-      fillTimeInSeconds: 128,
-    }
+  async getQuote(request: QuoteBridgeRequest): Promise<AcrossQuoteResult> {
+    const { sellTokenAddress, sellTokenChainId, buyTokenChainId, amount, recipient } = request
+    const suggestedFees = await this.api.getSuggestedFees({
+      token: sellTokenAddress,
+      // inputToken: sellTokenAddress,
+      // outputToken: buyTokenAddress,
+      originChainId: sellTokenChainId.toString(),
+      destinationChainId: buyTokenChainId.toString(),
+      amount,
+      recipient,
+    })
+
+    // TODO: The suggested fees contain way more information. As we review more bridge providers we should revisit the
+    // facade of the quote result.
+    //
+    // For example, this contains also information on the limits, so you don't need to quote again for the same pair.
+    // potentially, this could be cached for a short period of time in the SDK so we can resolve quotes with less
+    // requests.
+
+    return toBridgeQuoteResult(amount, suggestedFees)
   }
 
   async getUnsignedBridgeTx(_request: QuoteBridgeRequest, _quote: BridgeQuoteResult): Promise<EvmCall> {
@@ -107,28 +131,4 @@ export class AcrossBridgeProvider implements BridgeProvider<BridgeQuoteResult> {
   async getRefundBridgingTx(_bridgingId: string): Promise<EvmCall> {
     throw new Error('Not implemented')
   }
-}
-
-function getChainConfigs(
-  sourceChainId: SupportedChainId,
-  targetChainId: SupportedChainId
-): { sourceChainConfig: AcrossChainConfig; targetChainConfig: AcrossChainConfig } | undefined {
-  const sourceChainConfig = getChainConfig(sourceChainId)
-  const targetChainConfig = getChainConfig(targetChainId)
-
-  if (!sourceChainConfig || !targetChainConfig) return
-
-  return { sourceChainConfig, targetChainConfig }
-}
-
-function getChainConfig(chainId: number): AcrossChainConfig | undefined {
-  return Object.values(acrossTokenMapping).find((config) => config.chainId === chainId)
-}
-
-function getTokenSymbol(tokenAddress: string, chainConfig: AcrossChainConfig): string | undefined {
-  return Object.keys(chainConfig.tokens).find((key) => chainConfig.tokens[key] === tokenAddress)
-}
-
-function getTokenAddress(tokenSymbol: string, chainConfig: AcrossChainConfig): string | undefined {
-  return chainConfig.tokens[tokenSymbol as keyof AcrossChainConfig] as string | undefined
 }
---
+++ src/bridging/providers/across/util.ts
@@ -0,0 +1,65 @@
+import { SupportedChainId } from '../../../chains'
+import { SuggestedFeesResponse } from './acrossApi'
+import { AcrossQuoteResult } from './AcrossBridgeProvider'
+import { AcrossChainConfig, acrossTokenMapping } from './const/tokens'
+
+export function getChainConfigs(
+  sourceChainId: SupportedChainId,
+  targetChainId: SupportedChainId
+): { sourceChainConfig: AcrossChainConfig; targetChainConfig: AcrossChainConfig } | undefined {
+  const sourceChainConfig = getChainConfig(sourceChainId)
+  const targetChainConfig = getChainConfig(targetChainId)
+
+  if (!sourceChainConfig || !targetChainConfig) return
+
+  return { sourceChainConfig, targetChainConfig }
+}
+
+function getChainConfig(chainId: number): AcrossChainConfig | undefined {
+  return Object.values(acrossTokenMapping).find((config) => config.chainId === chainId)
+}
+
+export function getTokenSymbol(tokenAddress: string, chainConfig: AcrossChainConfig): string | undefined {
+  return Object.keys(chainConfig.tokens).find((key) => chainConfig.tokens[key] === tokenAddress)
+}
+
+export function getTokenAddress(tokenSymbol: string, chainConfig: AcrossChainConfig): string | undefined {
+  return chainConfig.tokens[tokenSymbol as keyof AcrossChainConfig] as string | undefined
+}
+
+export function toBridgeQuoteResult(amount: string, suggestedFees: SuggestedFeesResponse): AcrossQuoteResult {
+  // TODO: Do we need to use all the other fees, or they are included in totalRelayFee? I know 'lpFee' fee is, as stated in the docs, but not sure about the others.
+  // const relayerCapitalFee = suggestedFees.relayerCapitalFee
+  // const relayerGasFee = suggestedFees.relayerGasFee
+  // const lpFee = suggestedFees.lpFee
+
+  return {
+    suggestedFees,
+    buyAmount: applyFee(amount, suggestedFees.totalRelayFee.pct),
+    feeBps: pctToBps(suggestedFees.totalRelayFee.pct),
+    slippageBps: 0, // TODO: Can I set slippage to zero? are quotes enforced?
+  }
+}
+
+/**
+ * pct is a string that represents a percentage.
+ *
+ * Note: 1% is represented as 1e16, 100% is 1e18, 50% is 5e17, etc. These values are in the same format that the contract understands.
+ *
+ * Bps is a percentage in basis points (1/100th of a percent). For example, 1% is 100 bps.
+ *
+ */
+export function pctToBps(pct: string): number {
+  const pctBigInt = BigInt(pct)
+  return Number((pctBigInt * 10_000n) / 10n ** 18n)
+}
+
+export function applyFee(amount: string, pct: string): string {
+  const amountBigInt = BigInt(amount)
+  const pctBigInt = BigInt(pct)
+
+  // Compute amount after fee: amount * (1 - pct / 1e18)
+  const amountAfterFee = (amountBigInt * (10n ** 18n - pctBigInt)) / 10n ** 18n
+
+  return amountAfterFee.toString()
+}
