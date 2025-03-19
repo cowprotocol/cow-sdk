38370b717b5e17f0466178b12d994161df7c6b0b
Anxo Rodriguez <anxolin@gmail.com>
2025-03-19 13:29:49 +0000

    chore: use more specific imports

--- src/common/utils/wallet.ts
+++ src/common/utils/wallet.ts
@@ -1,9 +1,9 @@
-import { ethers, Signer } from 'ethers'
 import { Web3Provider } from '@ethersproject/providers'
 import { SignerLike } from '../types/wallets'
-
+import { Signer } from '@ethersproject/abstract-signer'
+import { Wallet } from '@ethersproject/wallet'
 export function getSigner(signer: SignerLike): Signer {
-  if (typeof signer === 'string') return new ethers.Wallet(signer)
+  if (typeof signer === 'string') return new Wallet(signer)
 
   if ('request' in signer || 'send' in signer) {
     const provider = new Web3Provider(signer)
