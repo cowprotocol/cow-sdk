15b63f3d073aa5a267f8bf712093183d024ae62e
Anxo Rodriguez <anxolin@gmail.com>
2025-03-20 10:35:08 +0000

    docs: added some documentation to the costs

--- src/order-book/types.ts
+++ src/order-book/types.ts
@@ -37,10 +37,47 @@ export interface Costs<T> {
  * For buy-orders the partner fee is added on top of the sell amount after network costs.
  */
 export interface QuoteAmountsAndCosts<T = bigint> {
+  /**
+   * Whether the quote is a sell or buy order.
+   */
   isSell: boolean
+
+  /**
+   * Estimated costs of settling the order.
+   *
+   * Costs are only payed if the order is executed. They cover the concept of gas costs to pay the solver for settling
+   * your order onchain for you.
+   *
+   * They are payed in the sell token although for convenience, the costs data includes also the buy token
+   * so UIs can decide how to show it to the user.
+   */
   costs: Costs<T>
+
+  /**
+   * Amounts before network costs. This amount could be shown to the user to reflect how much they are expected to get
+   * before applying any costs or fees (if costs and fees are displayed separately).
+   */
   beforeNetworkCosts: Amounts<T>
+
+  /**
+   * Amounts after including network costs.
+   */
   afterNetworkCosts: Amounts<T>
+
+  /**
+   * Amounts after including partner fees (if any).
+   *
+   * This amount could be shown to the user, as the expected to receive amount already including any fees or costs.
+   */
   afterPartnerFees: Amounts<T>
+
+  /**
+   * Amounts after including the slippage tolerance.
+   *
+   * This is the minimum that the user will receive and the amount they will sign.
+   *
+   * It already accounts for all costs, fees and some slippage tolerance so they can execute the order even in the
+   * event of the buy token appreciating.
+   */
   afterSlippage: Amounts<T>
 }
