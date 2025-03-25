f963a27e56f73ead978fabdb2461067e2b61a257
Anxo Rodriguez <anxolin@gmail.com>
2025-03-25 13:27:43 +0000

    chore: rename ERC20_DECIMALS_ABI

--- src/bridging/BridgingSdk/getErc20Decimals.ts
+++ src/bridging/BridgingSdk/getErc20Decimals.ts
@@ -4,11 +4,11 @@ import { Signer } from '@ethersproject/abstract-signer'
 import { GetErc20Decimals } from '../types'
 import { TargetChainId } from '../../chains'
 
-const ERC20_DECIMALS_OF_ABI = ['function decimals() external view returns (uint8)'] as const
+const ERC20_DECIMALS_ABI = ['function decimals() external view returns (uint8)'] as const
 
 export function factoryGetErc20Decimals(provider: Signer | Provider): GetErc20Decimals {
   return (_chainId: TargetChainId, tokenAddress: string) => {
-    const contract = new Contract(tokenAddress, ERC20_DECIMALS_OF_ABI, provider)
+    const contract = new Contract(tokenAddress, ERC20_DECIMALS_ABI, provider)
     return contract.decimals()
   }
 }
