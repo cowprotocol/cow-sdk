15dbb62728c2d467bfc0989b410717a27b69d077
Anxo Rodriguez <anxolin@gmail.com>
2025-03-26 16:39:40 +0000

    fix: wrong nesting

--- src/bridging/providers/across/AcrossApi.spec.ts
+++ src/bridging/providers/across/AcrossApi.spec.ts
@@ -9,32 +9,30 @@ describe('AcrossApi: Shape of API response', () => {
     api = new AcrossApi()
   })
 
-  describe('getAvailableRoutes', () => {
-    it('getSuggestedFees', async () => {
-      // Attempt to make a REAL API call. The API implementation will assert the result shape matches the expected object
-      //  Example: https://app.across.to/api/suggested-fees?token=0x82aF49447D8a07e3bd95BD0d56f35241523fBab1&originChainId=42161&destinationChainId=8453&amount=2389939424141418&recipient=0x016f34D4f2578c3e9DFfC3f2b811Ba30c0c9e7f3
-      const result = await api.getSuggestedFees({
-        token: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
-        originChainId: SupportedChainId.ARBITRUM_ONE,
-        destinationChainId: SupportedChainId.BASE,
-        amount: 2389939424141418n,
-        recipient: '0x016f34D4f2578c3e9DFfC3f2b811Ba30c0c9e7f3',
-      })
-
-      expect(result).toBeDefined()
+  it('getSuggestedFees', async () => {
+    // Attempt to make a REAL API call. The API implementation will assert the result shape matches the expected object
+    //  Example: https://app.across.to/api/suggested-fees?token=0x82aF49447D8a07e3bd95BD0d56f35241523fBab1&originChainId=42161&destinationChainId=8453&amount=2389939424141418&recipient=0x016f34D4f2578c3e9DFfC3f2b811Ba30c0c9e7f3
+    const result = await api.getSuggestedFees({
+      token: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1',
+      originChainId: SupportedChainId.ARBITRUM_ONE,
+      destinationChainId: SupportedChainId.BASE,
+      amount: 2389939424141418n,
+      recipient: '0x016f34D4f2578c3e9DFfC3f2b811Ba30c0c9e7f3',
     })
 
-    it('getAvailableRoutes', async () => {
-      // Attempt to make a REAL API call. The API implementation will assert the result shape matches the expected object
-      const result = await api.getAvailableRoutes({
-        originChainId: SupportedChainId.ARBITRUM_ONE.toString(),
-        originToken: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1', // weth
+    expect(result).toBeDefined()
+  })
 
-        destinationChainId: SupportedChainId.BASE.toString(),
-        destinationToken: '0x4200000000000000000000000000000000000006', // weth
-      })
+  it('getAvailableRoutes', async () => {
+    // Attempt to make a REAL API call. The API implementation will assert the result shape matches the expected object
+    const result = await api.getAvailableRoutes({
+      originChainId: SupportedChainId.ARBITRUM_ONE.toString(),
+      originToken: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1', // weth
 
-      expect(result).toBeDefined()
+      destinationChainId: SupportedChainId.BASE.toString(),
+      destinationToken: '0x4200000000000000000000000000000000000006', // weth
     })
+
+    expect(result).toBeDefined()
   })
 })
