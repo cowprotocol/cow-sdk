37e2a337382008a2a68b81b6c3ba3f66dfd91281
daniele <daniele@cow.fi>
2025-05-13 11:34:11 +0100

    chore: add Across Spook contract references

--- src/bridging/providers/across/const/contracts.ts
+++ src/bridging/providers/across/const/contracts.ts
@@ -1,10 +1,15 @@
 import { AdditionalTargetChainId, SupportedChainId, TargetChainId } from '../../../../chains'
 
 export const ACROSS_SPOOK_CONTRACT_ADDRESSES: Record<TargetChainId, string | undefined> = {
+  // https://docs.across.to/reference/contract-addresses/mainnet-chain-id-1
   [SupportedChainId.MAINNET]: '0x5c7BCd6E7De5423a257D81B442095A1a6ced35C5',
+  // https://docs.across.to/reference/contract-addresses/arbitrum-chain-id-42161-1
   [SupportedChainId.ARBITRUM_ONE]: '0xe35e9842fceaca96570b734083f4a58e8f7c5f2a',
+  // https://docs.across.to/reference/contract-addresses/base-chain-id-8453
   [SupportedChainId.BASE]: '0x09aea4b2242abC8bb4BB78D537A67a245A7bEC64',
+  // // https://docs.across.to/reference/contract-addresses/mainnet-chain-id-1
   [SupportedChainId.SEPOLIA]: '0x5ef6C01E11889d86803e0B23e3cB3F9E9d97B662',
+  // https://docs.across.to/reference/contract-addresses/polygon-chain-id-137
   [SupportedChainId.POLYGON]: '0x9295ee1d8C5b022Be115A2AD3c30C72E34e7F096',
 
   // Not supported chains
