3ac1920c55e3c7f27f2663d2a45b954494178cc4
Anxo Rodriguez <anxolin@gmail.com>
2025-05-16 18:58:30 +0100

    test: fix tests

--- src/trading/suggestSlippageFromFee.test.ts
+++ src/trading/suggestSlippageFromFee.test.ts
@@ -9,22 +9,27 @@ interface SuggestedFeeTest {
 }
 
 assertCases('handle different factors', [
-  { fee: 20n, factor: 0, expected: 0 },
-  { fee: 20n, factor: 1, expected: 1 },
-  { fee: 20n, factor: 25, expected: 5 },
-  { fee: 20n, factor: 50, expected: 10 },
-  { fee: 20n, factor: 75, expected: 15 },
-  { fee: 20n, factor: 100, expected: 20 },
-  { fee: 20n, factor: 200, expected: 40 },
-  { fee: 20n, factor: 100_000_000, expected: 20_000_000 },
+  { fee: 20n, factor: 0, expected: 0 }, // round(20*0) = 0
+  { fee: 20n, factor: 1, expected: 0 }, // round(20*0.01) = 0
+  { fee: 20n, factor: 2, expected: 0 }, // round(20*0.02) = 0
+  { fee: 20n, factor: 3, expected: 1 }, // round(20*0.03) = 1
+  { fee: 20n, factor: 25, expected: 5 }, // round(20*0.25) = 5
+  { fee: 20n, factor: 50, expected: 10 }, // round(20*0.5) = 10
+  { fee: 20n, factor: 75, expected: 15 }, // round(20*0.75) = 15
+  { fee: 20n, factor: 100, expected: 20 }, // round(20*1) = 20
+  { fee: 20n, factor: 200, expected: 40 }, // round(20*2) = 40
+  { fee: 20n, factor: 100_000_000, expected: 20_000_000 }, // round(20*1) = 20
 ])
 
 assertCases('Handle atoms', [
-  { fee: atoms(0.2), factor: 0, expected: 0 },
-  { fee: atoms(0.2), factor: 25, expected: 50000000000000000 },
-  { fee: atoms(0.2), factor: 50, expected: 100000000000000000 },
-  { fee: atoms(0.2), factor: 75, expected: 150000000000000000 },
-  { fee: atoms(0.2), factor: 100, expected: 200000000000000000 },
+  { fee: atoms(0.2), factor: 0, expected: 0 }, // round(0.2*1e18*0) = 0
+  { fee: atoms(0.2), factor: 1, expected: 2000000000000000 }, // round(0.2*1e18*0.01) = 2,000,000,000,000,000
+  { fee: atoms(0.2), factor: 2, expected: 4000000000000000 }, // round(0.2*1e18*0.02) = 4,000,000,000,000,000
+  { fee: atoms(0.2), factor: 3, expected: 6000000000000000 }, // round(0.2*1e18*0.03) = round(0.2*1e18*0.03)
+  { fee: atoms(0.2), factor: 25, expected: 50000000000000000 }, // round(0.2*1e18*0.25) = 50000000000000000
+  { fee: atoms(0.2), factor: 50, expected: 100000000000000000 }, // round(0.2*1e18*0.5) = 100000000000000000
+  { fee: atoms(0.2), factor: 75, expected: 150000000000000000 }, // round(0.2*1e18*0.75) = 150000000000000000
+  { fee: atoms(0.2), factor: 100, expected: 200000000000000000 }, // round(0.2*1e18*1) = 200000000000000000
 ])
 
 assertCases('Handle fee edge cases', [
--- src/trading/suggestSlippageFromVolume.test.ts
+++ src/trading/suggestSlippageFromVolume.test.ts
@@ -16,12 +16,14 @@ describe('Sell orders', () => {
   const isSell = true
 
   assertCases('handle different factors', isSell, [
-    { ...baseParams, percentage: 0, expected: 0 },
-    { ...baseParams, percentage: 0.5, expected: 1 },
-    { ...baseParams, percentage: 10, expected: 2 },
-    { ...baseParams, percentage: 25, expected: 4 },
-    { ...baseParams, percentage: 50, expected: 8 },
-    { ...baseParams, percentage: 100, expected: 15 },
+    { ...baseParams, percentage: 0, expected: 0 }, // round(15*0) = 0
+    { ...baseParams, percentage: 0.5, expected: 0 }, // round(15*0.005) = 0
+    { ...baseParams, percentage: 3, expected: 0 }, // round(15*0.03) = 0
+    { ...baseParams, percentage: 4, expected: 1 }, // round(15*0.04) = 1
+    { ...baseParams, percentage: 10, expected: 2 }, // round(15*0.1) = 2
+    { ...baseParams, percentage: 25, expected: 4 }, // round(15*0.25) = 4
+    { ...baseParams, percentage: 50, expected: 8 }, // round(15*0.5) = 8
+    { ...baseParams, percentage: 100, expected: 15 }, // round(15*1) = 15
     { ...baseParams, percentage: 100_000_000, expected: 15_000_000 },
   ])
 
@@ -55,13 +57,16 @@ describe('Buy orders', () => {
   const isSell = false
 
   assertCases('handle different factors', isSell, [
-    { ...baseParams, percentage: 0, expected: 0 },
-    { ...baseParams, percentage: 0.5, expected: 1 },
-    { ...baseParams, percentage: 10, expected: 2 },
-    { ...baseParams, percentage: 25, expected: 5 },
-    { ...baseParams, percentage: 50, expected: 10 },
-    { ...baseParams, percentage: 100, expected: 20 },
-    { ...baseParams, percentage: 100_000_000, expected: 20_000_000 },
+    { ...baseParams, percentage: 0, expected: 0 }, // round(20*0) = 0
+    { ...baseParams, percentage: 0.5, expected: 0 }, // round(20*0.005) = 0
+    { ...baseParams, percentage: 2.4, expected: 0 }, // round(20*0.024) = 0
+    { ...baseParams, percentage: 2.5, expected: 1 }, // round(20*0.025) = 1
+    { ...baseParams, percentage: 2.6, expected: 1 }, // round(20*0.026) = 1
+    { ...baseParams, percentage: 10, expected: 2 }, // round(20*0.1) = 2
+    { ...baseParams, percentage: 25, expected: 5 }, // round(20*0.25) = 5
+    { ...baseParams, percentage: 50, expected: 10 }, // round(20*0.5) = 10
+    { ...baseParams, percentage: 100, expected: 20 }, // round(20*1) = 20
+    { ...baseParams, percentage: 100_000_000, expected: 20_000_000 }, // round(20*1) = 20
   ])
 
   assertCases('Handle atoms', isSell, [
