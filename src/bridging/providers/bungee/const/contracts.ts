e0c8942d52b39b39a66456ab99a41f6f3c90ccb0
Sebastian T F <sebastian.tharakan97@gmail.com>
2025-06-03 23:54:04 +0530

    chore: add contract addresses

--- src/bridging/providers/bungee/const/contracts.ts
+++ src/bridging/providers/bungee/const/contracts.ts
@@ -1,25 +1,25 @@
-// TODO deploy on all chains
 import { AdditionalTargetChainId, SupportedChainId, TargetChainId } from '../../../../chains'
 
+const BUNGEE_COWSWAP_LIB_ADDRESS = '0x411b20d4593fee999b37bdb4a9d0ff862bdc0c77'
 export const BungeeCowswapLibAddresses: Record<TargetChainId, string | undefined> = {
-  [SupportedChainId.MAINNET]: undefined,
-  [SupportedChainId.GNOSIS_CHAIN]: undefined,
-  [SupportedChainId.ARBITRUM_ONE]: '0x73eb30778f7e3958bfd974d10c0be559c2c65e22',
-  [SupportedChainId.BASE]: undefined,
-  [SupportedChainId.AVALANCHE]: undefined,
-  [SupportedChainId.POLYGON]: undefined,
+  [SupportedChainId.MAINNET]: BUNGEE_COWSWAP_LIB_ADDRESS,
+  [SupportedChainId.GNOSIS_CHAIN]: BUNGEE_COWSWAP_LIB_ADDRESS,
+  [SupportedChainId.ARBITRUM_ONE]: BUNGEE_COWSWAP_LIB_ADDRESS,
+  [SupportedChainId.BASE]: BUNGEE_COWSWAP_LIB_ADDRESS,
+  [SupportedChainId.AVALANCHE]: BUNGEE_COWSWAP_LIB_ADDRESS,
+  [SupportedChainId.POLYGON]: BUNGEE_COWSWAP_LIB_ADDRESS,
   [SupportedChainId.SEPOLIA]: undefined,
-  [AdditionalTargetChainId.OPTIMISM]: undefined,
+  [AdditionalTargetChainId.OPTIMISM]: BUNGEE_COWSWAP_LIB_ADDRESS,
 }
 
-// TODO deploy on all chains
+const SOCKET_VERIFIER_ADDRESS = '0xa27A3f5A96DF7D8Be26EE2790999860C00eb688D'
 export const SocketVerifierAddresses: Record<TargetChainId, string | undefined> = {
-  [SupportedChainId.MAINNET]: undefined,
-  [SupportedChainId.GNOSIS_CHAIN]: undefined,
-  [SupportedChainId.ARBITRUM_ONE]: '0x69D9f76e4cbE81044FE16C399387b12e4DBF27B1',
-  [SupportedChainId.BASE]: undefined,
-  [SupportedChainId.AVALANCHE]: undefined,
-  [SupportedChainId.POLYGON]: undefined,
+  [SupportedChainId.MAINNET]: SOCKET_VERIFIER_ADDRESS,
+  [SupportedChainId.GNOSIS_CHAIN]: SOCKET_VERIFIER_ADDRESS,
+  [SupportedChainId.ARBITRUM_ONE]: SOCKET_VERIFIER_ADDRESS,
+  [SupportedChainId.BASE]: SOCKET_VERIFIER_ADDRESS,
+  [SupportedChainId.AVALANCHE]: SOCKET_VERIFIER_ADDRESS,
+  [SupportedChainId.POLYGON]: SOCKET_VERIFIER_ADDRESS,
   [SupportedChainId.SEPOLIA]: undefined,
-  [AdditionalTargetChainId.OPTIMISM]: undefined,
+  [AdditionalTargetChainId.OPTIMISM]: SOCKET_VERIFIER_ADDRESS,
 }
