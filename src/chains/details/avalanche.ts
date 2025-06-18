69e6637c18ad2472d602bc235a3dccd1888fa75d
cowdan <daniele@cow.fi>
2025-06-18 11:31:07 +0100

    feat(avalanche): change Avalanche explorer from Snowtrace to Snowscan (#357)
    * feat: change Avalanche explorer from Snowtrace to Snowscan
    
    * chore: address CR comment

--- src/chains/details/avalanche.ts
+++ src/chains/details/avalanche.ts
@@ -40,7 +40,7 @@ export const avalanche: ChainInfo = {
     url: 'https://build.avax.network/docs',
   },
   blockExplorer: {
-    name: 'Snowtrace',
-    url: 'https://snowtrace.io',
+    name: 'Snowscan',
+    url: 'https://snowscan.xyz',
   },
 }
