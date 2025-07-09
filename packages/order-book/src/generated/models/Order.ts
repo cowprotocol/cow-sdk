007b35fd199d4f0ceef37c0ee3315146b8483b46
Jefferson Bastos <jeff.junior98@gmail.com>
2025-07-09 15:12:47 -0300

    fix(order-book): fix command for auto-generated folder in order-book

--- packages/order-book/src/generated/models/Order.ts
+++
@@ -1,8 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-import type { OrderCreation } from './OrderCreation';
-import type { OrderMetaData } from './OrderMetaData';
-export type Order = (OrderCreation & OrderMetaData);
-
