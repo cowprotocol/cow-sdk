72c581149b75823df18cd688c61cf0681f89c81a
Jefferson Bastos <jeff.junior98@gmail.com>
2025-07-16 17:47:20 -0300

    chore(sdk): FIx importSchema app-data

--- packages/app-data/src/importSchema.ts
+++ packages/app-data/src/importSchema.ts
@@ -1,30 +1,14 @@
 import { CowError } from '@cowprotocol/sdk-common'
 import { AnyAppDataDocVersion } from './generatedTypes'
-import * as fs from 'fs'
-import * as path from 'path'
-
-const SCHEMAS_DIR = path.resolve(process.cwd(), 'schemas')
-const schemaCache: Record<string, AnyAppDataDocVersion> = {}
 
 export async function importSchema(version: string): Promise<AnyAppDataDocVersion> {
   if (!/^\d+\.\d+\.\d+$/.test(version)) {
     throw new Error(`AppData version ${version} is not a valid version`)
   }
-
-  if (schemaCache[version]) {
-    return schemaCache[version]
-  }
-
   try {
-    const schemaPath = path.join(SCHEMAS_DIR, `v${version}.json`)
-
-    const content = fs.readFileSync(schemaPath, 'utf8')
-    const schema = JSON.parse(content) as AnyAppDataDocVersion
-
-    schemaCache[version] = schema
-
-    return schema
-  } catch {
+    return await import(`../schemas/v${version}.json`)
+  } catch (e) {
+    console.log(e)
     throw new CowError(`AppData version ${version} doesn't exist`)
   }
 }
