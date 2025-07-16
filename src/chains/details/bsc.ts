1fa74ff12aedd220334d3bc6db61722926435f6a
Alfetopito <alfetopito@users.noreply.github.com>
2025-07-16 17:22:17 +0100

    feat: add chain info for lens and bsc

---
+++ src/chains/details/bsc.ts
@@ -0,0 +1,47 @@
+import { nativeCurrencyTemplate } from '../../common/consts/tokens'
+import { RAW_CHAINS_FILES_PATH } from '../const/path'
+import { ChainInfo, SupportedChainId } from '../types'
+
+const bscLogo = `${RAW_CHAINS_FILES_PATH}/images/bsc-logo.svg` // TODO: Add BSC logo
+
+// See https://github.com/wevm/viem/blob/main/src/chains/definitions/bsc.ts
+// and https://github.com/ethereum-lists/chains/blob/master/_data/chains/eip155-56.json
+export const bsc: ChainInfo = {
+  id: SupportedChainId.BSC,
+  label: 'BSC',
+  eip155Label: 'BNB Smart Chain Mainnet',
+  logo: { light: bscLogo, dark: bscLogo },
+  nativeCurrency: {
+    ...nativeCurrencyTemplate,
+    chainId: SupportedChainId.BSC,
+    name: 'BNB Chain Native Token',
+    symbol: 'BNB',
+    logoUrl: bscLogo, // TODO: use BNB logo
+  },
+  addressPrefix: 'bnb',
+  isTestnet: false,
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 15921452,
+    },
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://bsc-dataseed1.bnbchain.org'],
+    },
+  },
+  color: '#F0B90B',
+  website: {
+    name: 'BNB Chain',
+    url: 'https://www.bnbchain.org',
+  },
+  docs: {
+    name: 'BNB Chain Docs',
+    url: 'https://docs.bnbchain.org',
+  },
+  blockExplorer: {
+    name: 'BNB Smart Chain Explorer',
+    url: 'https://bscscan.com',
+  },
+}
---
+++ src/chains/details/lens.ts
@@ -0,0 +1,49 @@
+import { nativeCurrencyTemplate } from '../../common/consts/tokens'
+import { RAW_CHAINS_FILES_PATH } from '../const/path'
+import { ChainInfo, SupportedChainId } from '../types'
+
+const lensLogo = `${RAW_CHAINS_FILES_PATH}/images/lens-logo.svg` // TODO: Add Lens logo
+
+// See https://github.com/wevm/viem/blob/main/src/chains/definitions/lens.ts
+// and https://github.com/ethereum-lists/chains/blob/master/_data/chains/eip155-232.json
+export const lens: ChainInfo = {
+  id: SupportedChainId.LENS,
+  label: 'Lens',
+  eip155Label: 'Lens Mainnet',
+  logo: { light: lensLogo, dark: lensLogo },
+  nativeCurrency: {
+    ...nativeCurrencyTemplate,
+    chainId: SupportedChainId.LENS,
+    name: 'GHO',
+    symbol: 'GHO',
+    logoUrl: lensLogo, // TODO: use GHO logo
+  },
+  addressPrefix: 'lens',
+  isTestnet: false,
+  contracts: {
+    multicall3: {
+      address: '0xca11bde05977b3631167028862be2a173976ca11',
+      blockCreated: 1724216,
+    },
+  },
+  rpcUrls: {
+    default: {
+      http: ['https://rpc.lens.xyz'],
+    },
+  },
+  color: '#F0B90B', // TODO: add color
+  website: {
+    name: 'Lens',
+    url: 'https://www.lens.xyz',
+  },
+  docs: {
+    name: 'Lens Docs',
+    url: 'https://docs.lens.xyz',
+  },
+  blockExplorer: {
+    name: 'Lens Explorer',
+    url: 'https://explorer.lens.xyz',
+    // Note: doesn't seem to be Etherscan derived. Will need to consider it in the code.
+    // TODO: add a flag to indicate that the explorer is not Etherscan derived and handle it in the code.
+  },
+}
