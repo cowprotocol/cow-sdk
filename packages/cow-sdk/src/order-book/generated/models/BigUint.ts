81a75ad9539b3894f73e839c09c54c8083d24fd6
jeffersonBastos <51352654+jeffersonBastos@users.noreply.github.com>
2025-07-25 11:05:58 -0300

    chore(sdk-agnostic-lib): cleanup legacy files after order-book package migration (#380)
    * chore(bridging): copy of bridging package
    
    * chore(bridging): copy of hooks/utils and test utils
    
    * feat(bridging): refactor bridging package to support multiple adapters, fix ContractFactory, create decodeFunctionData and fix InterfaceWrappers
    
    * feat(bridging): refactor cowShed to accept optional adapter in constructor
    
    * feat(bridging): update pnpm-loc.yaml
    
    * fix(order-book): fix command for auto-generated folder in order-book
    
    * chore(order-book): update pnpm-lock.yaml
    
    * chore(config): cleanup legacy files after package migration
    
    * chore(common): cleanup legacy files after package migration
    
    * chore(order-book): cleanup legacy files after package migration

--- packages/cow-sdk/src/order-book/generated/models/BigUint.ts
+++
@@ -1,8 +0,0 @@
-/* generated using openapi-typescript-codegen -- do not edit */
-/* istanbul ignore file */
-/* tslint:disable */
-/* eslint-disable */
-/**
- * A big unsigned integer encoded in decimal.
- */
-export type BigUint = string;
