c1216802810f56899b6d78a1d25bae591fce7e9a
jeffersonBastos <51352654+jeffersonBastos@users.noreply.github.com>
2025-07-25 11:05:47 -0300

    chore(sdk-agnostic-lib): cleanup legacy files after common package migration (#379)
    * chore(bridging): copy of bridging package
    
    * chore(bridging): copy of hooks/utils and test utils
    
    * feat(bridging): refactor bridging package to support multiple adapters, fix ContractFactory, create decodeFunctionData and fix InterfaceWrappers
    
    * feat(bridging): refactor cowShed to accept optional adapter in constructor
    
    * feat(bridging): update pnpm-loc.yaml
    
    * fix(order-book): fix command for auto-generated folder in order-book
    
    * chore(order-book): update pnpm-lock.yaml
    
    * chore(config): cleanup legacy files after package migration
    
    * chore(common): cleanup legacy files after package migration

--- packages/cow-sdk/src/common/types/wallets.ts
+++
@@ -1,6 +0,0 @@
-import { Signer } from 'ethers'
-import type { ExternalProvider } from '@ethersproject/providers'
-
-export type PrivateKey = string // 64 characters
-export type AccountAddress = `0x${string}` // 42 characters
-export type SignerLike = Signer | ExternalProvider | PrivateKey
