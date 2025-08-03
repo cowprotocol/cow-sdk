56b2a9c5e243454b9c25067dc70600777d26ae26
Jefferson Bastos <jeff.junior98@gmail.com>
2025-08-03 20:47:43 -0300

    chore: roolback stringfy lazy import

--- packages/app-data/src/utils/stringify.ts
+++ packages/app-data/src/utils/stringify.ts
@@ -1,4 +1,5 @@
+import stringify from 'json-stringify-deterministic'
+
 export async function stringifyDeterministic(obj: object): Promise<string> {
-  const stringify = (await import('json-stringify-deterministic')).default
   return stringify(obj)
 }
