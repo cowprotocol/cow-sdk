442258a01864f3fc501a87588ef23d4e32bc48c8
Jefferson Bastos <jeff.junior98@gmail.com>
2025-08-25 14:40:24 -0300

    fix: adjust eslint config

--- examples/react/ethers5/eslint.config.js
+++ examples/react/ethers5/eslint.config.js
@@ -18,6 +18,10 @@ export default tseslint.config([
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+      parserOptions: {
+        project: ['./tsconfig.node.json', './tsconfig.app.json'],
+        tsconfigRootDir: import.meta.dirname,
+      },
     },
   },
 ])
