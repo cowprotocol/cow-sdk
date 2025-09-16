f8e736aafb6c3ca2c2020282e0f1af7ce6c0b5ac
Alexandr Kazachenko <shoom3301@gmail.com>
2025-09-16 14:11:19 +0500

    fix(bridge): update bungee smart-contract address (#506)

--- packages/bridging/src/providers/bungee/const/contracts.ts
+++ packages/bridging/src/providers/bungee/const/contracts.ts
@@ -28,8 +28,7 @@ export const SocketVerifierAddresses: Record<TargetChainId, string | undefined> 
   [AdditionalTargetChainId.OPTIMISM]: SOCKET_VERIFIER_ADDRESS,
 }
 
-// TODO: replace with the actual address after deployment
-const BUNGEE_APPROVE_AND_BRIDGE_V1_ADDRESS = '0x936fa1cfd96849329B18b915773E176718A64b95'
+const BUNGEE_APPROVE_AND_BRIDGE_V1_ADDRESS = '0xD06a673fe1fa27B1b9E5BA0be980AB15Dbce85cc'
 export const BungeeApproveAndBridgeV1Addresses: Record<TargetChainId, string | undefined> = {
   [SupportedChainId.MAINNET]: BUNGEE_APPROVE_AND_BRIDGE_V1_ADDRESS,
   [SupportedChainId.GNOSIS_CHAIN]: BUNGEE_APPROVE_AND_BRIDGE_V1_ADDRESS,
