070be99ac39f73a4352c45b1803ed987cd9e57dc
Alessandro Manfredi <alle.manfredi@gmail.com>
2025-09-25 13:49:25 +0200

    refactor(packages/bridging/src/providers/near-intents): replaces WRAPPED_NATIVE_CURRENCIES with the one within @cowprotocol/sdk-config

--- packages/bridging/src/providers/near-intents/const/index.ts
+++ packages/bridging/src/providers/near-intents/const/index.ts
@@ -1,6 +1,7 @@
 import { arbitrumOne, avalanche, base, bnb, gnosisChain, mainnet, optimism, polygon } from '@cowprotocol/sdk-config'
+
 import { HOOK_DAPP_BRIDGE_PROVIDER_PREFIX } from '../../../const'
-import type { Hex } from 'viem'
+
 import { BridgeStatus } from '../../../types'
 
 export const NEAR_INTENTS_HOOK_DAPP_ID = `${HOOK_DAPP_BRIDGE_PROVIDER_PREFIX}/near-intents`
@@ -39,17 +40,6 @@ export const CHAIN_ID_NEAR_INTENTS_BLOCKCHAIN = {
 } as const satisfies Record<NearBlockchainKey, number>
 
 export type NearBlockchainKey = keyof typeof NEAR_INTENTS_BLOCKCHAIN_CHAIN_IDS
-
-export const WRAPPED_NATIVE_CURRENCIES = {
-  [arbitrumOne.id]: '0x82aF49447D8a07e3bd95BD0d56f35241523fBab1', // weth on arb
-  [avalanche.id]: '0xB31f66AA3C1e785363F0875A1B74E27b85FD66c7', // wavax on avax
-  [base.id]: '0x4200000000000000000000000000000000000006', // weth on base
-  [bnb.id]: '0xbb4CdB9CBd36B01bD1cBaEBF2De08d9173bc095c', // wbnb on bsc
-  [mainnet.id]: '0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2', // weth on eth
-  [gnosisChain.id]: '0xe91D153E0b41518A2Ce8Dd3D7944Fa863463a97d', // wxdai on gnosis
-  [optimism.id]: '0x4200000000000000000000000000000000000006', // weth on op
-  [polygon.id]: '0x0d500B1d8E8eF31E21C99d1Db9A6444d3ADf1270', // wpol on pol
-} as const satisfies Record<number, Hex>
 
 export const NEAR_INTENTS_STATUS_TO_COW_STATUS: Record<string, BridgeStatus> = {
   KNOWN_DEPOSIT_TX: BridgeStatus.IN_PROGRESS,
