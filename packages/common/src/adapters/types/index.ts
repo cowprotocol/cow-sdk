66a2efa0710b9f52ac45a9a7c4de622cbc164bdf
Anxo Rodriguez <anxolin@gmail.com>
2025-10-29 11:13:03 +0000

    feat: add from/to fields for receipt

--- packages/common/src/adapters/types/index.ts
+++ packages/common/src/adapters/types/index.ts
@@ -72,6 +72,8 @@ export interface Log {
  * Standardized transaction receipt
  */
 export interface TransactionReceipt {
+  from: string
+  to: string
   transactionHash: string
   blockNumber: bigint
   blockHash: string
