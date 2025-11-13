c351a756d09d706363f40b02b587b5f4f396acc0
Alexandr Kazachenko <shoom3301@gmail.com>
2025-11-13 19:55:53 +0500

    feat(flash-loans): update smartcontract addresses
    Refactor AAVE adapter factory and hooks to use mapAddressToSupportedNetworks for initialization.

--- packages/flash-loans/src/aave/const.ts
+++ packages/flash-loans/src/aave/const.ts
@@ -23,31 +23,19 @@ export const AAVE_POOL_ADDRESS: Record<SupportedChainId, string> = {
   [SupportedChainId.LINEA]: '0xc47b8C00b0f69a36fa203Ffeac0334874574a8Ac',
   [SupportedChainId.PLASMA]: '0x925a2A7214Ed92428B5b1B090F80b25700095e12',
 }
-export const AAVE_ADAPTER_FACTORY: Record<SupportedChainId, string> = {
-  ...mapAddressToSupportedNetworks(''),
-  [SupportedChainId.GNOSIS_CHAIN]: '0x7f230F7Cee38ca371b871B74B3C6ded4932A2f93',
-  [SupportedChainId.MAINNET]: '0x22E08c56a6799e28e7b05A117D853B9b46abc017',
-  [SupportedChainId.BASE]: '0xc5D68e305e0b5998f895e34d4440954072F285B6',
-}
+export const AAVE_ADAPTER_FACTORY: Record<SupportedChainId, string> = mapAddressToSupportedNetworks(
+  '0x43c658Ea38bBfD897706fDb35e2468ef5D8F6927',
+)
 
-const AAVE_COLLATERAL_SWAP_ADAPTER_HOOK: Record<SupportedChainId, string> = {
-  ...mapAddressToSupportedNetworks(''),
-  [SupportedChainId.GNOSIS_CHAIN]: '0x837aA74e8daf7Fd7160f078ca455a67B7F441E4b',
-  [SupportedChainId.MAINNET]: '0xFEb471EC22E5572dbb44229301c266f4C305A78a',
-  [SupportedChainId.BASE]: '0xBb45A7898A6f06a9c148BfeD0C103140F0079cd9',
-}
-const AAVE_DEBT_SWAP_ADAPTER_HOOK: Record<SupportedChainId, string> = {
-  ...mapAddressToSupportedNetworks(''),
-  [SupportedChainId.GNOSIS_CHAIN]: '0xC9A495A17B1eeC18AE0f0865840B8fd3f8a9DE3F',
-  [SupportedChainId.MAINNET]: '0x238f57A2c3F0696fB20295075a0F9A18EfC67D3a',
-  [SupportedChainId.BASE]: '0xaCbd34fAB78BD6C8738eb32dDAFd688df98CD2E3',
-}
-const AAVE_REPAY_COLLATERAL_ADAPTER_HOOK: Record<SupportedChainId, string> = {
-  ...mapAddressToSupportedNetworks(''),
-  [SupportedChainId.GNOSIS_CHAIN]: '0x2890CE372f3a397B6f3BB3a71B1836A6F3F33657',
-  [SupportedChainId.MAINNET]: '0x9eB0ffd318e862D344792a8e589e8393E8bEd96F',
-  [SupportedChainId.BASE]: '0x1549445700D0Cb2D7Ce85ECd5a7FD7Ba4a3D40A7',
-}
+const AAVE_COLLATERAL_SWAP_ADAPTER_HOOK: Record<SupportedChainId, string> = mapAddressToSupportedNetworks(
+  '0x29A9b0a13c81d59f13BA0f39DBDCAA1AB2adc95F',
+)
+const AAVE_DEBT_SWAP_ADAPTER_HOOK: Record<SupportedChainId, string> = mapAddressToSupportedNetworks(
+  '0xbE9A121bb958BBBb027dA728DEC0D5496811b7d1',
+)
+const AAVE_REPAY_COLLATERAL_ADAPTER_HOOK: Record<SupportedChainId, string> = mapAddressToSupportedNetworks(
+  '0x8e25d1210FabB0fcAdE92a82C4a89568B4b10E0F',
+)
 
 export const AAVE_HOOK_ADAPTER_PER_TYPE: Record<AaveFlashLoanType, Record<SupportedChainId, string>> = {
   [AaveFlashLoanType.CollateralSwap]: AAVE_COLLATERAL_SWAP_ADAPTER_HOOK,
