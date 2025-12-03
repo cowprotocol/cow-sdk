795374f5a93d273d954d87fd869eb8209ee97031
Daniel Constantin <hello@danielconstantin.net>
2025-12-03 14:59:59 +0200

    fix: address code rabbit feedback

--- packages/config/src/constants/paths.ts
+++ packages/config/src/constants/paths.ts
@@ -1,5 +1,5 @@
-// Base path to GitHub raw files
+// Base CDN path for SDK files
 export const RAW_FILES_PATH = 'https://files.cow.fi/cow-sdk'
 
-// for chains
+// CDN path for chain data
 export const RAW_CHAINS_FILES_PATH = `${RAW_FILES_PATH}/chains`
