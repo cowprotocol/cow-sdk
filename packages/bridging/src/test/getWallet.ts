a00dbbd6a26238bcee2d4452487d16551560c59f
Alexandr Kazachenko <shoom3301@gmail.com>
2026-02-02 22:03:40 +0500

    chore: revert revert Ink network (#789)

--- packages/bridging/src/test/getWallet.ts
+++ packages/bridging/src/test/getWallet.ts
@@ -11,6 +11,7 @@ import {
   plasma,
   polygon,
   sepolia,
+  ink,
   SupportedChainId,
 } from '@cowprotocol/sdk-config'
 
@@ -26,6 +27,7 @@ const DEFAULT_RPC_URL: Record<SupportedChainId, string | undefined> = {
   [SupportedChainId.BNB]: bnb.rpcUrls.default.http[0],
   [SupportedChainId.LINEA]: linea.rpcUrls.default.http[0],
   [SupportedChainId.PLASMA]: plasma.rpcUrls.default.http[0],
+  [SupportedChainId.INK]: ink.rpcUrls.default.http[0],
 }
 
 export async function getRpcProvider(chainId: SupportedChainId) {
