e54e81ff1116a10e0f29c301c71a67bc618d6156
limitofzero <limitofzero@gmail.com>
2026-02-18 18:36:15 +0400

    feat: rename fn

--- packages/config/src/chains/const/utils.ts
+++ packages/config/src/chains/const/utils.ts
@@ -1,10 +1,22 @@
-import { ALL_SUPPORTED_CHAIN_IDS } from './chainIds'
-import { SupportedChainId } from '../types'
+import { ALL_CHAINS_IDS, ALL_SUPPORTED_CHAIN_IDS } from './chainIds'
+import { SupportedChainId, TargetChainId } from '../types'
 
 export function mapSupportedNetworks<T>(value: (chainId: SupportedChainId) => T): Record<SupportedChainId, T>
 export function mapSupportedNetworks<T>(value: T): Record<SupportedChainId, T>
 export function mapSupportedNetworks<T>(value: T | ((chainId: SupportedChainId) => T)): Record<SupportedChainId, T> {
   return ALL_SUPPORTED_CHAIN_IDS.reduce<Record<number, T>>(
+    (acc, chainId) => ({
+      ...acc,
+      [chainId]: typeof value === 'function' ? (value as (chainId: SupportedChainId) => T)(chainId) : value,
+    }),
+    {},
+  )
+}
+
+export function mapAllNetworks<T>(value: (chainId: TargetChainId) => T): Record<TargetChainId, T>
+export function mapAllNetworks<T>(value: T): Record<TargetChainId, T>
+export function mapAllNetworks<T>(value: T | ((chainId: TargetChainId) => T)): Record<TargetChainId, T> {
+  return ALL_CHAINS_IDS.reduce<Record<number, T>>(
     (acc, chainId) => ({
       ...acc,
       [chainId]: typeof value === 'function' ? (value as (chainId: SupportedChainId) => T)(chainId) : value,
