51a65e73ff2da9b44f217b44f1ebb606655a7211
shoom3301 <shoom3301@gmail.com>
2026-03-10 21:17:09 +0500

    fix: fix settlementContractOverride

--- packages/trading/src/postCoWProtocolTrade.ts
+++ packages/trading/src/postCoWProtocolTrade.ts
@@ -39,6 +39,7 @@ export async function postCoWProtocolTrade(
   const { appDataKeccak256, fullAppData } = appData
 
   const env = params.env ?? orderBookApi.context.env
+  const settlementContractOverride = params.settlementContractOverride
   const chainId = orderBookApi.context.chainId
   const from = owner || (await signer.getAddress())
 
@@ -65,7 +66,13 @@ export async function postCoWProtocolTrade(
         }
       }
 
-      const signingResult = await OrderSigningUtils.signOrder(orderToSign, chainId, signer, env)
+      const signingResult = await OrderSigningUtils.signOrder(
+        orderToSign,
+        chainId,
+        signer,
+        env,
+        settlementContractOverride,
+      )
 
       if (isEip1271) {
         return {
