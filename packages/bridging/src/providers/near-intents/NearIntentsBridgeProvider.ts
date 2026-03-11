b610e287e97f0cdb1caaafb81314432ad455d6ae
limitofzero <limitofzero@gmail.com>
2026-03-11 12:41:11 +0400

    feat: remove check

--- packages/bridging/src/providers/near-intents/NearIntentsBridgeProvider.ts
+++ packages/bridging/src/providers/near-intents/NearIntentsBridgeProvider.ts
@@ -18,7 +18,6 @@ import {
 import { adaptToken, adaptTokens, calculateDeadline, getTokenByAddressAndChainId, hashQuote } from './util'
 
 import type { AbstractProviderAdapter } from '@cowprotocol/sdk-common'
-import { isEvmChain } from '@cowprotocol/sdk-config'
 import type { ChainId, ChainInfo, EvmCall, SupportedChainId, TokenInfo } from '@cowprotocol/sdk-config'
 import type { CowShedSdkOptions } from '@cowprotocol/sdk-cow-shed'
 import type { QuoteResponse } from '@defuse-protocol/one-click-sdk-typescript'
@@ -261,9 +260,9 @@ export class NearIntentsBridgeProvider implements ReceiverAccountBridgeProvider<
     }
 
     // Ensure chain IDs are supported for BridgingDepositParams
-    if (!isEvmChain(adaptedInput.chainId) || !isEvmChain(adaptedOutput.chainId)) {
-      throw new Error('Non-EVM chains are not supported for BridgingDepositParams')
-    }
+    // if (!isEvmChain(adaptedInput.chainId) || !isEvmChain(adaptedOutput.chainId)) {
+    //   throw new Error('Non-EVM chains are not supported for BridgingDepositParams')
+    // }
 
     // Build response
     return {
