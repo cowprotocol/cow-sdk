63b958252d77079aa5c90c95b1313322e89665d8
leohhhn <hudakleon@gmail.com>
2026-04-30 13:32:11 +0200

    add allowance check in viem example

--- examples/nodejs/viem/src/index.ts
+++ examples/nodejs/viem/src/index.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config'
-import { createPublicClient, http } from 'viem'
+import { createPublicClient, Hash, http } from 'viem'
 import { privateKeyToAccount } from 'viem/accounts'
 import { sepolia } from 'viem/chains'
 import {
@@ -10,11 +10,13 @@ import {
   WRAPPED_NATIVE_CURRENCIES,
 } from '@cowprotocol/cow-sdk'
 import { ViemAdapter } from '@cowprotocol/sdk-viem-adapter'
+import { log } from 'console'
 
 // =================== Config ===================
-const RPC_URL = 'https://sepolia.gateway.tenderly.co'
-const PRIVATE_KEY = '0x' // private key here (0x...)
-const DEFAULT_SELL_AMOUNT = 0.1 // WETH amount
+const RPC_URL = 'https://ethereum-sepolia-public.nodies.app'
+const PRIVATE_KEY = '' // private key here (0x...)
+const DEFAULT_SELL_AMOUNT = 0.01 // WETH amount
+
 // ===============================================================
 
 async function main() {
@@ -47,6 +49,30 @@ async function main() {
   const owner = account.address
   const amount = BigInt(Math.floor(DEFAULT_SELL_AMOUNT * 10 ** WETH.decimals)) // 0.1 WETH
   const slippageBps = 50
+
+  const approvedAmount = await sdk.getCowProtocolAllowance({
+    tokenAddress: WETH.address,
+    owner,
+  })
+
+  console.log('Checking ...')
+  if (approvedAmount < amount) {
+    console.log('Approving Cow Protocol...')
+    try {
+      const approvalTxHash = await sdk.approveCowProtocol({
+        tokenAddress: WETH.address,
+        amount: amount,
+      })
+
+      const res = await publicClient.waitForTransactionReceipt({ hash: approvalTxHash as Hash })
+      console.log('Approved:', res)
+    } catch (e) {
+      console.error('Approval tx failed:', e)
+      process.exit(1)
+    }
+  } else {
+    console.log('Already approved Cow Protocol for swap amount')
+  }
 
   console.log('Owner:', owner)
   console.log('Getting quote...')
